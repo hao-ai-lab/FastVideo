a9baf01d02be1802de42606f411ddda983ca8d86
rlsu9 <a1286225768@gmail.com>
2024-11-11 00:10:31 +0400

    add LADD discriminator

--- fastvideo/distill.py
+++ fastvideo/distill.py
@@ -31,7 +31,7 @@ import numpy as np
 from fastvideo.model.modeling_mochi import MochiTransformer3DModel
 from diffusers.utils import check_min_version
 from fastvideo.dataset.latent_datasets import LatentDataset, latent_collate_function
-from fastvideo.gan.discriminator import DummyDiscriminator
+from fastvideo.gan.discriminator import LADDDiscriminator
 import torch.distributed as dist
 from safetensors.torch import save_file
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
@@ -174,7 +174,7 @@ def train_one_step_generator(student_transformer, teacher_transformer, discrimin
 
     grad_norm = student_transformer.clip_grad_norm_(max_grad_norm)
     student_optimizer.step()
-    return total_loss, grad_norm
+    return total_loss, grad_norm.item()
 
 def train_one_step_discriminator(student_transformer, teacher_transformer, discriminator, discriminator_optimizer, loader,noise_scheduler, gradient_accumulation_steps, sp_size,  max_grad_norm, weighting_scheme, logit_mean, logit_std, mode_scale):
     total_loss = 0.0
@@ -185,15 +185,16 @@ def train_one_step_discriminator(student_transformer, teacher_transformer, discr
     for _ in range(gradient_accumulation_steps):
         latents, encoder_hidden_states, latents_attention_mask, encoder_attention_mask = next(loader)
         latents = normalize_mochi_dit_input(latents)
-        with torch.inference_mode():
+        with torch.no_grad():
             student_pred = student_forward(student_transformer, latents, encoder_hidden_states, encoder_attention_mask, sp_size)
             teacher_features_fake = teacher_forward(teacher_transformer, student_pred, encoder_hidden_states, encoder_attention_mask, sp_size,  noise_scheduler, weighting_scheme, logit_mean, logit_std, mode_scale)
-            teacher_features_real = teacher_forward(teacher_transformer, latents, encoder_hidden_states, encoder_attention_mask, sp_size,  noise_scheduler, weighting_scheme, logit_mean, logit_std, mode_scale)
-        fake_logits = discriminator(teacher_features_fake)
-        real_logits = discriminator(teacher_features_real)
-        fake_loss = (F.relu(torch.ones_like(fake_logits) - fake_logits)).mean() 
-        real_loss = (F.relu(torch.ones_like(real_logits) + real_logits)).mean()
-        loss = fake_loss + real_loss
+            teacher_features_real = teacher_forward(teacher_transformer, latents,      encoder_hidden_states, encoder_attention_mask, sp_size,  noise_scheduler, weighting_scheme, logit_mean, logit_std, mode_scale)
+        fake_logits = discriminator(teacher_features_fake, encoder_hidden_states)
+        real_logits = discriminator(teacher_features_real, encoder_hidden_states)
+        fake_loss = (F.relu(torch.ones_like(fake_logits) + fake_logits)).mean() 
+        real_loss = (F.relu(torch.ones_like(real_logits) - real_logits)).mean()
+
+        loss = (fake_loss + real_loss)/2
         
         loss.backward()
         
@@ -204,7 +205,7 @@ def train_one_step_discriminator(student_transformer, teacher_transformer, discr
 
     grad_norm = discriminator.clip_grad_norm_(max_grad_norm)
     discriminator_optimizer.step()
-    return total_loss, grad_norm
+    return total_loss, grad_norm.item()
         
     
 def main(args):
@@ -250,7 +251,11 @@ def main(args):
         subfolder="transformer",
         load_dtype=torch.bfloat16, # TODO: FP8 or INT8 weight type
     )
-    discriminator = DummyDiscriminator(3072, 48)
+    # feature_dim, cnn_channel_size, text_cond_dim, num_layers, layers_stride):
+    discriminator = LADDDiscriminator(feature_dim=3072,  text_cond_dim=4096, num_layers=48, layers_stride=4)
+    # get num of discriminator parameters in billion
+    main_print(f"--> discriminator parameters: {sum(p.numel() for p in discriminator.parameters()) / 1e9} B")
+    
     teacher_transformer.requires_grad_(False)
     
     
@@ -359,10 +364,11 @@ def main(args):
     loader = sp_parallel_dataloader_wrapper(train_dataloader, device, args.train_batch_size, args.sp_size, args.train_sp_batch_size)
     
     for step in range(1, args.max_train_steps+1):
-        generator_loss, student_grad_norm= train_one_step_generator(student_transformer, teacher_transformer, discriminator, \
-            student_optimizer, loader, noise_scheduler, args.gradient_accumulation_steps, args.sp_size,  \
-                args.max_grad_norm, args.weighting_scheme, args.logit_mean, args.logit_std, args.mode_scale)
-        
+        # generator_loss, student_grad_norm= train_one_step_generator(student_transformer, teacher_transformer, discriminator, \
+        #     student_optimizer, loader, noise_scheduler, args.gradient_accumulation_steps, args.sp_size,  \
+        #         args.max_grad_norm, args.weighting_scheme, args.logit_mean, args.logit_std, args.mode_scale)
+        generator_loss = 0
+        student_grad_norm = 0
         discriminator_loss, discriminator_grad_norm = train_one_step_discriminator(student_transformer, teacher_transformer, discriminator, \
             discriminator_optimizer, loader, noise_scheduler, args.gradient_accumulation_steps, args.sp_size,  \
                 args.max_grad_norm, args.weighting_scheme, args.logit_mean, args.logit_std, args.mode_scale)
--- fastvideo/gan/discriminator.py
+++ fastvideo/gan/discriminator.py
@@ -3,7 +3,7 @@ import torch
 from torch import nn
 import numpy as np
 from torch.nn.utils.parametrizations import spectral_norm
-
+import os 
 class DummyDiscriminator(nn.Module):
     def __init__(self, dim_in, num_layers):
         super().__init__()
@@ -28,7 +28,7 @@ class ResidualBlock(nn.Module):
         return (self.fn(x) + x) / np.sqrt(2)
     
     
-class SpectralConv1d():
+class SpectralConv1d(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.conv = spectral_norm(nn.Conv1d(*args, **kwargs))
@@ -72,24 +72,24 @@ def make_block(channels: int, kernel_size: int) -> nn.Module:
         nn.LeakyReLU(0.2, True),
     )
     
-class LADDDiscriminator(nn.Module):
-    def __init__(self, channels: int, text_c_dim: int, cmap_dim: int = 64):
+class DiscHead(nn.Module):
+    def __init__(self, feature_dim: int, text_c_dim: int, cmap_dim: int = 64, cnn_dim=512):
         super().__init__()
-        self.channels = channels
+        self.channels = feature_dim
         self.text_c_dim = text_c_dim
         self.cmap_dim = cmap_dim
-
+        self.down_proj = SpectralConv1d(feature_dim, cnn_dim, kernel_size=1, padding=0)
         self.main = nn.Sequential(
-            make_block(channels, kernel_size=1),
-            ResidualBlock(make_block(channels, kernel_size=9))
+            make_block(cnn_dim, kernel_size=1),
+            ResidualBlock(make_block(cnn_dim, kernel_size=9))
         )
 
-
         self.cmapper = nn.Linear(self.text_c_dim, cmap_dim)
-        self.cls = SpectralConv1d(channels, cmap_dim, kernel_size=1, padding=0)
+        self.cls = SpectralConv1d(cnn_dim, cmap_dim, kernel_size=1, padding=0)
 
     def forward(self, x: torch.Tensor, c: torch.Tensor) -> torch.Tensor:
-        h = self.main(x)
+        h = self.down_proj(x)
+        h = self.main(h)
         out = self.cls(h)
 
         cmap = self.cmapper(c).unsqueeze(-1)
@@ -97,3 +97,27 @@ class LADDDiscriminator(nn.Module):
 
         return out
 
+
+class LADDDiscriminator(nn.Module):
+    def __init__(self, feature_dim, text_cond_dim, num_layers, layers_stride):
+        super().__init__()
+        heads = []
+        for i in range(0, num_layers, layers_stride):
+            heads.append(DiscHead(feature_dim, text_cond_dim))
+        self.heads = nn.ModuleList(heads)
+        self.layers_stride = layers_stride
+        self.num_layers = num_layers
+        
+    def forward(self, features, text_conditions) -> torch.Tensor:
+        text_conditions = text_conditions.mean(1)
+        # layer, B, L, C -> layer, B, C, L
+        features = features.transpose(2, 3)
+        logits = []
+        for i in range(0, self.num_layers, self.layers_stride):
+            head = self.heads[i//self.layers_stride]
+            feat = features[i]
+            logits.append(head(feat, text_conditions).view(feat.size(0), -1))
+        logits = torch.cat(logits, dim=1)
+
+
+        return logits
