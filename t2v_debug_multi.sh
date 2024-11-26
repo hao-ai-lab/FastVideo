f23c302af47051bc0a911e59fb9b6794e742730e
rlsu9 <a1286225768@gmail.com>
2024-11-26 11:49:42 +0400

    runlong script

--- t2v_debug_multi.sh
+++ t2v_debug_multi.sh
@@ -10,12 +10,12 @@ torchrun --nnodes 1 --nproc_per_node 4\
     --validation_prompt_dir "data/Encoder_Overfit_Data/validation_prompt_embed_mask"\
     --uncond_prompt_dir "data/BLACK-MYTH-YQ/uncond_prompt_embed_mask"\
     --gradient_checkpointing\
-    --train_batch_size=8\
-    --num_latent_t 1\
-    --sp_size 1\
-    --train_sp_batch_size 1\
+    --train_batch_size=1\
+    --num_latent_t 16\
+    --sp_size 4\
+    --train_sp_batch_size 4\
     --dataloader_num_workers 4\
-    --gradient_accumulation_steps=1\
+    --gradient_accumulation_steps=2\
     --max_train_steps=20000\
     --learning_rate=1e-6\
     --mixed_precision="bf16"\
