fix precision issue according to HF

fastvideo/sample/sample_t2v_mochi.py
@@ -112,9 +112,9 @@ def main(args):
         linear_quadratic = True if args.scheduler_type == "pcm_linear_quadratic" else False
         scheduler = PCMFMDeterministicScheduler(1000, args.shift, args.num_euler_timesteps, linear_quadratic)
     if args.transformer_path is not None:
-        transformer = MochiTransformer3DModel.from_pretrained(args.transformer_path, torch_dtype=torch.bfloat16)
+        transformer = MochiTransformer3DModel.from_pretrained(args.transformer_path)
     else:
-        transformer = MochiTransformer3DModel.from_pretrained(args.model_path, subfolder = 'transformer/', torch_dtype=torch.bfloat16)
+        transformer = MochiTransformer3DModel.from_pretrained(args.model_path, subfolder = 'transformer/')
     if args.lora_checkpoint_dir is not None:
         # Load and merge LoRA weights
         transformer = load_lora_checkpoint(
@@ -123,11 +123,12 @@ def main(args):
             output_dir=args.lora_checkpoint_dir
         )
         print(f"Loaded and merged LoRA weights from {args.lora_checkpoint_dir}")
-    pipe = MochiPipeline.from_pretrained(args.model_path, transformer = transformer,scheduler=scheduler, torch_dtype=torch.bfloat16)
+    pipe = MochiPipeline.from_pretrained(args.model_path, transformer = transformer,scheduler=scheduler)
     
     pipe.enable_vae_tiling()
-    pipe.to(device)
-    #pipe.enable_model_cpu_offload()
+    # pipe.to(device)
+    
+    pipe.enable_model_cpu_offload(device)
     # Generate videos from the input prompt
 
     if args.prompt_embed_path is not None:
@@ -145,9 +146,23 @@ def main(args):
         
     if prompts is not None:
         videos = []
-        for prompt in prompts:
-            video = pipe(
-                prompt=[prompt],
+        with torch.autocast("cuda", dtype=torch.bfloat16):
+                for prompt in prompts:
+                    video = pipe(
+                        prompt=[prompt],
+                        height=args.height,
+                        width=args.width,
+                        num_frames=args.num_frames,
+                        num_inference_steps=args.num_inference_steps,
+                        guidance_scale=args.guidance_scale,
+                        generator=generator,
+                    ).frames
+                    videos.append(video[0])
+    else:
+        with torch.autocast("cuda", dtype=torch.bfloat16):
+            videos = pipe(
+                prompt_embeds=prompt_embeds,
+                prompt_attention_mask=encoder_attention_mask,
                 height=args.height,
                 width=args.width,
                 num_frames=args.num_frames,
@@ -155,18 +170,6 @@ def main(args):
                 guidance_scale=args.guidance_scale,
                 generator=generator,
             ).frames
-            videos.append(video[0])
-    else:
-        videos = pipe(
-            prompt_embeds=prompt_embeds,
-            prompt_attention_mask=encoder_attention_mask,
-            height=args.height,
-            width=args.width,
-            num_frames=args.num_frames,
-            num_inference_steps=args.num_inference_steps,
-            guidance_scale=args.guidance_scale,
-            generator=generator,
-        ).frames
 
     if nccl_info.global_rank <= 0:
         if prompts is not None:

fastvideo/utils/validation.py
@@ -133,14 +133,16 @@ def sample_validation_video(
                 return_dict=False,
             )[0]
             
-
+            # Mochi CFG + Sampling runs in FP32
+            noise_pred = noise_pred.to(torch.float32)
             if do_classifier_free_guidance:
                 noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
                 noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
             # compute the previous noisy sample x_t -> x_t-1
             latents_dtype = latents.dtype
-            latents = scheduler.step(noise_pred, t, latents, return_dict=False)[0]
+            latents = scheduler.step(noise_pred, t, latents.to(torch.float32), return_dict=False)[0]
+            latents = latents.to(latents_dtype)
 
             if latents.dtype != latents_dtype:
                 if torch.backends.mps.is_available():