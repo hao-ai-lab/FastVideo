f4395bb4ab08050faeb2ef3041dbcb93834593d3
rlsu9 <a1286225768@gmail.com>
2024-11-28 03:07:51 +0400

    merge

--- inference_mochi.sh
+++ inference_mochi.sh
@@ -6,16 +6,15 @@ torchrun --nnodes=1 --nproc_per_node=$num_gpus --master_port 29503 \
     fastvideo/sample/sample_t2v_mochi.py \
     --model_path data/mochi \
     --prompt_path data/prompt.txt \
-    --transformer_path data/outputs/video_distill_synthetic/checkpoint-250\
     --num_frames  163 \
     --height 480 \
     --width 848 \
-    --num_inference_steps 8 \
+    --num_inference_steps 64 \
     --guidance_scale 4.5 \
-    --output_path outputs_video/distill_lq_163_250/ \
+    --output_path outputs_video/distill_baseline_debug_fp32cast/ \
     --shift 4 \
     --seed 12345 \
-    --scheduler_type "pcm_linear_quadratic" 
+    --scheduler_type "euler" 
 
 
 
