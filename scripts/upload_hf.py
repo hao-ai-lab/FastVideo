bf415331903065fe8fc04f194ccf2cecbe9f0768
Yongqi Chen <yongqich@gl-login2.arc-ts.umich.edu>
2024-11-27 19:55:27 -0500

    ignore preprocess scripts

--- scripts/upload_hf.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi
-api = HfApi()
-
-api.upload_folder(
-    folder_path="data/Mochi-Synthetic-Data",
-    repo_id="Stealths-Video/Mochi-Synthetic-Data",
-    repo_type="dataset",
-)
