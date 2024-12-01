5a4c22757250f66e491735d95d8818dd1a4998af
rlsu9 <a1286225768@gmail.com>
2024-12-02 00:50:03 +0400

    update

--- scripts/experiment_17-node1.sh
+++ scripts/experiment_17-node1.sh
@@ -31,7 +31,7 @@ torchrun --nnodes 1 --nproc_per_node 8\
     --train_sp_batch_size 1\
     --dataloader_num_workers 4\
     --gradient_accumulation_steps=1\
-    --max_train_steps=4000\
+    --max_train_steps=2000\
     --learning_rate=1e-6\
     --mixed_precision="bf16"\
     --checkpointing_steps=500\
