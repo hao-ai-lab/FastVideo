98070d9bcae4e91c329571f9635a91534b65f0f0
rlsu9 <r3su@ucsd.edu>
2024-12-12 19:54:22 +0000

    add val gen

--- scripts/preprocess_hunyuan_data.sh
+++ scripts/preprocess_hunyuan_data.sh
@@ -4,9 +4,9 @@ MODEL_PATH="data/hunyuan"
 MODEL_TYPE="hunyuan"
 DATA_MERGE_PATH="data/Mixkit-All-Clips/merge.txt"
 OUTPUT_DIR="data/Hunyuan-Mixkit-Data"
-
+VALIDATION_PATH="assets/prompt.txt"
 # torchrun --nproc_per_node=$GPU_NUM \
-#     ./fastvideo/data_preprocess/finetune_data_VAE.py \
+#     fastvideo/data_preprocess/preprocess_vae_latents.py \
 #     --model_path $MODEL_PATH \
 #     --data_merge_path $DATA_MERGE_PATH \
 #     --train_batch_size=1 \
@@ -21,7 +21,16 @@ OUTPUT_DIR="data/Hunyuan-Mixkit-Data"
 
 
 torchrun --nproc_per_node=$GPU_NUM \
-    ./fastvideo/data_preprocess/finetune_data_text_encoder.py \
+    fastvideo/data_preprocess/preprocess_text_embeddings.py \
     --model_type $MODEL_TYPE \
     --model_path $MODEL_PATH \
     --output_dir=$OUTPUT_DIR 
+
+
+
+torchrun --nproc_per_node=1 \
+    fastvideo/data_preprocess/preprocess_validation_text_embeddings.py \
+    --model_type $MODEL_TYPE \
+    --model_path $MODEL_PATH \
+    --output_dir=$OUTPUT_DIR \
+    --validation_prompt_txt $VALIDATION_PATH
