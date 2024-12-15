7dbf677baaa72c93eb9bd12a76b48679e1447c35
foreverpiano <pianoqwz@qq.com>
2024-12-15 04:05:39 +0000

    env

---
+++ env_setup.sh
@@ -0,0 +1,10 @@
+
+
+
+
+conda create -n fastvideo python=3.10.0 -y && conda activate fastvideo
+pip3 install torch==2.5.0 torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install packaging ninja && pip install flash-attn==2.7.0.post2 --no-build-isolation 
+pip install "git+https://github.com/huggingface/diffusers.git@bf64b32652a63a1865a0528a73a13652b201698b"
+
+pip install -e .
