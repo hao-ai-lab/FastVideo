3edb674e0de9ca423c2d122bd8c71939127eff09
rlsu9 <r3su@ucsd.edu>
2024-12-15 08:14:31 +0000

    update

--- scripts/hunyuan_experiments_19.sh
+++ scripts/hunyuan_experiments_19.sh
@@ -10,12 +10,12 @@ export NCCL_PROTO=simple
 DATA_DIR=/data
 IP=10.4.139.86
 
-torchrun --nnodes 2 --nproc_per_node 8\
+torchrun --nnodes 4 --nproc_per_node 8\
     --node_rank=0 \
     --rdzv_id=456 \
     --rdzv_backend=c10d \
     --rdzv_endpoint=$IP:29500 \
-    fastvideo/distill_adv.py\
+    fastvideo/distill.py\
     --seed 42\
     --pretrained_model_name_or_path $DATA_DIR/hunyuan\
     --dit_model_name_or_path $DATA_DIR/hunyuan/hunyuan-video-t2v-720p/transformers/mp_rank_00_model_states.pt\
@@ -25,8 +25,8 @@ torchrun --nnodes 2 --nproc_per_node 8\
     --validation_prompt_dir "$DATA_DIR/HD-Mixkit-Finetune-HunYuan/validation"\
     --gradient_checkpointing\
     --train_batch_size=1\
-    --num_latent_t 32\
-    --sp_size 4 \
+    --num_latent_t 32 \
+    --sp_size 2 \
     --train_sp_batch_size 1\
     --dataloader_num_workers 4\
     --gradient_accumulation_steps=1\
@@ -41,12 +41,12 @@ torchrun --nnodes 2 --nproc_per_node 8\
     --ema_start_step 0\
     --cfg 0.0\
     --log_validation\
-    --output_dir="$DATA_DIR/outputs/hy_phase1_shift17_bs_8_adv_HD"\
+    --output_dir="$DATA_DIR/outputs/hy_phase1_shift33_bs_16_HD"\
     --tracker_project_name Hunyuan_Distill \
     --num_height 720 \
     --num_width 1280 \
     --num_frames  125 \
-    --shift 17 \
+    --shift 33 \
     --validation_guidance_scale "1.0" \
     --num_euler_timesteps 50 \
     --multi_phased_distill_schedule "4000-1" \
