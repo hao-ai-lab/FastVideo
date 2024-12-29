d25c235558702a3d0b36dc93ecd442ea53e8e458
“BrianChen1129” <yongqich@umich.edu>
2024-12-29 21:52:29 +0000

    unified hunyuan hf

--- scripts/inference/inference_hunyuan_hf.sh
+++ scripts/inference/inference_hunyuan_hf.sh
@@ -3,15 +3,15 @@
 num_gpus=4
 torchrun --nnodes=1 --nproc_per_node=$num_gpus --master_port 29503 \
     fastvideo/sample/sample_t2v_hunyuan_hf.py \
-    --model_path ~/data/hunyuan_diffusers/ \
+    --model_path data/hunyuan_diffusers/ \
     --prompt_path "assets/prompt.txt" \
     --num_frames 93 \
     --height 480 \
     --width 848 \
     --num_inference_steps 50 \
-    --guidance_scale 6 \
-    --output_path outputs_video/hunyuan_hf_test/ \
+    --output_path outputs_video/hunyuan_hf/ \
     --seed 1024 \
+
 
 
 
