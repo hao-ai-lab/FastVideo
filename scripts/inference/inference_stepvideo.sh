9937a9628124d37d131ff5157ce8934852afdf0a
peiyuan zhang <a1286225768@gmail.com>
2025-02-20 19:12:24 +0000

    update parallel count

--- scripts/inference/inference_stepvideo.sh
+++ scripts/inference/inference_stepvideo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=7 python fastvideo/sample/call_remote_server_stepvideo.py --model_dir data/stepvideo-t2v/ &
 
-parallel=6
+parallel=4
 url='127.0.0.1'
 model_dir=data/stepvideo-t2v
 torchrun --nproc_per_node $parallel fastvideo/sample/sample_t2v_stepvideo.py \
