5ab20afbea2c7139245916b02461078a63247015
Peiyuan Zhang <a1286225768@gmail.com>
2025-03-26 03:01:00 +0000

    debug numerical error

--- fastvideo/v1/models/encoders/llama.py
+++ fastvideo/v1/models/encoders/llama.py
@@ -338,9 +338,7 @@ class LlamaModel(nn.Module):
         for layer in self.layers:
             if output_hidden_states:
                 # TODO
-                all_hidden_states += (
-                    hidden_states, ) if residual is None else (hidden_states +
-                                                               residual, )
+                all_hidden_states += (hidden_states,) if residual is None else (hidden_states + residual,)
             hidden_states, residual = layer(positions, hidden_states, residual)
 
         hidden_states, _ = self.norm(hidden_states, residual)
