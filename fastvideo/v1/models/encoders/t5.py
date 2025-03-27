07bcc2c6b5a689938ff498758faef57f1daf46a5
JerryZhou54 <zhouw.jerry2017@outlook.com>
2025-03-27 04:43:10 +0000

    Fix activation

--- fastvideo/v1/models/encoders/t5.py
+++ fastvideo/v1/models/encoders/t5.py
@@ -17,33 +17,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PyTorch T5 model."""
+"""PyTorch T5 & UMT5 model."""
 
 import math
-import re
 from typing import Iterable, List, Optional, Set, Tuple
-import copy
 from dataclasses import dataclass
 
 import torch
 from torch import nn
 import torch.nn.functional as F
 from transformers import T5Config
-# TODO best way to handle xformers imports?
 
-# TODO func should be in backend interface
-# from vllm.attention.backends.xformers import (XFormersMetadata, _get_attn_bias,
-#                                               _set_attn_bias)
-# from vllm.attention.layer import Attention, AttentionMetadata
-# from vllm.config import CacheConfig, VllmConfig
 from fastvideo.v1.distributed import get_tensor_model_parallel_world_size
 from fastvideo.v1.layers.activation import get_act_fn
 from fastvideo.v1.layers.layernorm import RMSNorm
 from fastvideo.v1.layers.linear import (ColumnParallelLinear,
                                         QKVParallelLinear,
                                         RowParallelLinear)
-# from vllm.model_executor.layers.logits_processor import LogitsProcessor
-# from vllm.model_executor.layers.sampler import SamplerOutput, get_sampler
 from fastvideo.v1.layers.vocab_parallel_embedding import VocabParallelEmbedding
 from fastvideo.v1.models.loader.weight_utils import (
     default_weight_loader)
@@ -85,12 +75,6 @@ class T5DenseActDense(nn.Module):
     def forward(self, hidden_states) -> torch.Tensor:
         hidden_states, _ = self.wi(hidden_states)
         hidden_states = self.act(hidden_states)
-        # if (
-        #     isinstance(self.wo.weight, torch.Tensor)
-        #     and hidden_states.dtype != self.wo.weight.dtype
-        #     and self.wo.weight.dtype != torch.int8
-        # ):
-        #     hidden_states = hidden_states.to(self.wo.weight.dtype)
         hidden_states, _ = self.wo(hidden_states)
         return hidden_states
 
@@ -199,18 +183,8 @@ class T5Attention(nn.Module):
                                           bias=False,
                                           quant_config=quant_config)
 
-        # NOTE (NickLucche) T5 employs a scaled weight initialization scheme
-        # instead of scaling attention scores directly.
-        # self.attn = Attention(self.n_heads,
-        #                       config.d_kv,
-        #                       1.0,
-        #                       quant_config=quant_config,
-        #                       prefix=f"{prefix}.attn",
-        #                       attn_type=self.attn_type)
         self.attn = T5MultiHeadAttention()
 
-        # Only the first SelfAttention block in encoder decoder has this
-        # embedding layer, the others reuse its output.
         if self.has_relative_attention_bias:
             self.relative_attention_bias = \
                 VocabParallelEmbedding(self.relative_attention_num_buckets,
@@ -286,7 +260,6 @@ class T5Attention(nn.Module):
                      key_length,
                      device=None) -> torch.Tensor:
         """Compute binned relative position bias"""
-        # TODO possible tp issue?
         if device is None:
             device = self.relative_attention_bias.weight.device
         context_position = torch.arange(query_length,
@@ -334,12 +307,7 @@ class T5Attention(nn.Module):
             # Self-attention. Compute T5 relative positional encoding.
             # The bias term is computed on longest sequence in batch. Biases
             # for shorter sequences are slices of the longest.
-
-            # bias expected shape: (num_seqs, NH, L, L_pad) for prefill,
-            #                      (num_seqs, NH, 1, L_pad) for decodes.
             assert self.attn_type == AttentionType.ENCODER
-            # TODO (NickLucche) avoid extra copy on repeat,
-            # provide multiple slices of same memory
             attn_bias = self.compute_bias(seq_len, seq_len).repeat(num_seqs, 1, 1, 1)
             attn_metadata.attn_bias = attn_bias
         else:
@@ -520,80 +488,7 @@ class T5Stack(nn.Module):
         hidden_states = self.final_layer_norm.forward_native(hidden_states)
         return hidden_states
 
-
-# class T5Model(nn.Module):
-#     _tied_weights_keys = [
-#         "encoder.embed_tokens.weight", "decoder.embed_tokens.weight"
-#     ]
-
-#     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
-#         super().__init__()
-#         config: T5Config = vllm_config.model_config.hf_config
-#         cache_config = vllm_config.cache_config
-#         quant_config = vllm_config.quant_config
-#         lora_config = vllm_config.lora_config
-
-#         lora_vocab = (lora_config.lora_extra_vocab_size *
-#                       (lora_config.max_loras or 1)) if lora_config else 0
-#         self.vocab_size = config.vocab_size + lora_vocab
-#         self.padding_idx = config.pad_token_id
-#         self.shared = VocabParallelEmbedding(
-#             config.vocab_size,
-#             config.d_model,
-#             org_num_embeddings=config.vocab_size)
-
-#         self.encoder = T5Stack(config,
-#                                False,
-#                                config.num_layers,
-#                                self.shared,
-#                                cache_config=cache_config,
-#                                quant_config=quant_config,
-#                                prefix=f"{prefix}.encoder")
-#         self.decoder = T5Stack(config,
-#                                True,
-#                                config.num_decoder_layers,
-#                                self.shared,
-#                                cache_config=cache_config,
-#                                quant_config=quant_config,
-#                                prefix=f"{prefix}.decoder")
-
-#     def get_input_embeddings(self, input_ids: torch.Tensor) -> torch.Tensor:
-#         return self.shared(input_ids)
-
-#     def forward(self, input_ids: torch.Tensor, encoder_input_ids: torch.Tensor,
-#                 kv_caches: List[torch.Tensor],
-#                 attn_metadata: AttentionMetadata) -> torch.Tensor:
-#         encoder_hidden_states = None
-
-#         if encoder_input_ids.numel() > 0:
-#             # Run encoder attention if a non-zero number of encoder tokens
-#             # are provided as input: on a regular generate call, the encoder
-#             # runs once, on the prompt. Subsequent decoder calls reuse output
-#             # `encoder_hidden_states`.
-#             encoder_hidden_states = self.encoder(input_ids=encoder_input_ids,
-#                                                  kv_caches=kv_caches,
-#                                                  attn_metadata=attn_metadata)
-#             # Clear attention bias state.
-#             attn_metadata.attn_bias = None
-#             attn_metadata.encoder_attn_bias = None
-#             attn_metadata.cross_attn_bias = None
-
-#         decoder_outputs = self.decoder(
-#             input_ids=input_ids,
-#             encoder_hidden_states=encoder_hidden_states,
-#             kv_caches=kv_caches,
-#             attn_metadata=attn_metadata)
-
-#         # When capturing CUDA Graph
-#         attn_metadata.attn_bias = None
-#         attn_metadata.encoder_attn_bias = None
-#         attn_metadata.cross_attn_bias = None
-#         return decoder_outputs
-
 class T5EncoderModel(nn.Module):
-    _tied_weights_keys = ["encoder.embed_tokens.weight"]
-    _keys_to_ignore_on_load_unexpected = [r"decoder"]
-
     def __init__(self, config: T5Config, prefix: str = ""):
         super().__init__()
 
@@ -609,29 +504,11 @@ class T5EncoderModel(nn.Module):
             config.num_layers,
             self.shared,
             quant_config=quant_config,
-            prefix=f"{prefix}.encoder")
+            prefix=f"{prefix}.encoder",
+            is_umt5=False)
 
     def get_input_embeddings(self):
         return self.shared
-
-    def set_input_embeddings(self, new_embeddings):
-        self.shared = new_embeddings
-        self.encoder.set_input_embeddings(new_embeddings)
-
-    def _tie_weights(self):
-        if self.config.tie_word_embeddings:
-            self._tie_or_clone_weights(self.encoder.embed_tokens, self.shared)
-
-    def get_encoder(self):
-        return self.encoder
-
-    def _prune_heads(self, heads_to_prune):
-        """
-        Prunes heads of the model. heads_to_prune: dict of {layer_num: list of heads to prune in this layer} See base
-        class PreTrainedModel
-        """
-        for layer, heads in heads_to_prune.items():
-            self.encoder.block[layer].layer[0].SelfAttention.prune_heads(heads)
 
     def forward(
         self,
@@ -643,19 +520,59 @@ class T5EncoderModel(nn.Module):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> torch.Tensor:
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
+        attn_metadata = AttentionMetadata(None)
         encoder_outputs = self.encoder(
             input_ids=input_ids,
             attention_mask=attention_mask,
-            inputs_embeds=inputs_embeds,
-            head_mask=head_mask,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
+            attn_metadata=attn_metadata,
         )
 
         return encoder_outputs
+
+    def load_weights(self, weights: Iterable[Tuple[str,
+                                                   torch.Tensor]]) -> Set[str]:
+        stacked_params_mapping = [
+            # (param_name, shard_name, shard_id)
+            (".qkv_proj", ".q", "q"),
+            (".qkv_proj", ".k", "k"),
+            (".qkv_proj", ".v", "v"),
+        ]
+        params_dict = dict(self.named_parameters())
+        loaded_params: Set[str] = set()
+        for name, loaded_weight in weights:
+            loaded = False
+            if "decoder" in name or "lm_head" in name:
+                continue
+            for param_name, weight_name, shard_id in stacked_params_mapping:
+                if weight_name not in name:
+                    continue
+                name = name.replace(weight_name, param_name)
+                # Skip loading extra bias for GPTQ models.
+                if name.endswith(".bias") and name not in params_dict:
+                    continue
+
+                if name not in params_dict:
+                    continue
+                
+                param = params_dict[name]
+                weight_loader = param.weight_loader
+                weight_loader(param, loaded_weight, shard_id)
+                loaded = True
+                break
+            if not loaded:
+                # Skip loading extra bias for GPTQ models.
+                if name.endswith(".bias") and name not in params_dict:
+                    continue
+
+                if name not in params_dict:
+                    continue
+
+                param = params_dict[name]
+                weight_loader = getattr(param, "weight_loader",
+                                        default_weight_loader)
+                weight_loader(param, loaded_weight)
+            loaded_params.add(name)
+        return loaded_params
 
 class UMT5EncoderModel(nn.Module):
     def __init__(self, config: T5Config, prefix: str = ""):
@@ -742,128 +659,3 @@ class UMT5EncoderModel(nn.Module):
                 weight_loader(param, loaded_weight)
             loaded_params.add(name)
         return loaded_params
-
-# class T5ForConditionalGeneration(nn.Module):
-#     _keys_to_ignore_on_load_unexpected = [
-#         "decoder.block.0.layer.1.EncDecAttention.relative_attention_bias.weight",
-#     ]
-#     _tied_weights_keys = [
-#         "encoder.embed_tokens.weight", "decoder.embed_tokens.weight",
-#         "lm_head.weight"
-#     ]
-
-#     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
-#         super().__init__()
-#         config: T5Config = vllm_config.model_config.hf_config
-#         self.model_dim = config.d_model
-#         self.config = config
-#         self.unpadded_vocab_size = config.vocab_size
-#         if lora_config := vllm_config.lora_config:
-#             self.unpadded_vocab_size += lora_config.lora_extra_vocab_size
-
-#         self.model = T5Model(vllm_config=vllm_config,
-#                              prefix=maybe_prefix(prefix, "model"))
-#         # Although not in config, this is the default for hf models.
-#         if self.config.tie_word_embeddings:
-#             self.lm_head = self.model.shared
-#             # in transformers this is smt more explicit, as in (after load)
-#             # self.lm_head.weight = self.model.shared.weight
-#         else:
-#             self.lm_head = ParallelLMHead(self.unpadded_vocab_size,
-#                                           config.d_model,
-#                                           org_num_embeddings=config.vocab_size)
-
-#         self.logits_processor = LogitsProcessor(self.unpadded_vocab_size,
-#                                                 config.vocab_size)
-#         self.sampler = get_sampler()
-
-#     def compute_logits(
-#         self,
-#         hidden_states: torch.Tensor,
-#         sampling_metadata,
-#     ) -> Optional[torch.Tensor]:
-#         if self.config.tie_word_embeddings:
-#             # Rescale output before projecting on vocab
-#             # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586 # noqa: E501
-#             hidden_states = hidden_states * (self.model_dim**-0.5)
-#         logits = self.logits_processor(self.lm_head, hidden_states,
-#                                        sampling_metadata)
-#         return logits
-
-#     def sample(
-#         self,
-#         logits: Optional[torch.Tensor],
-#         sampling_metadata,
-#     ) -> Optional[SamplerOutput]:
-#         next_tokens = self.sampler(logits, sampling_metadata)
-#         return next_tokens
-
-#     def get_input_embeddings(self, input_ids: torch.Tensor) -> torch.Tensor:
-#         return self.model.shared(input_ids)
-
-#     def forward(
-#         self,
-#         input_ids: torch.Tensor,
-#         positions: torch.Tensor,
-#         kv_caches: List[torch.Tensor],
-#         attn_metadata: AttentionMetadata,
-#         intermediate_tensors: Optional[torch.Tensor] = None,
-#         *,
-#         encoder_input_ids: torch.Tensor,
-#         encoder_positions: torch.Tensor,
-#         **kwargs,
-#     ) -> torch.Tensor:
-#         return self.model(input_ids, encoder_input_ids, kv_caches,
-#                           attn_metadata)
-
-#     def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
-#         model_params_dict = dict(self.named_parameters(remove_duplicate=False))
-#         loaded_params: Set[str] = set()
-#         renamed_reg = [
-#             (re.compile(r'block\.(\d+)\.layer\.0'), r'blocks.\1.self_attn'),
-#             (re.compile(r'decoder.block\.(\d+)\.layer\.1'),
-#              r'decoder.blocks.\1.cross_attn'),
-#             (re.compile(r'decoder.block\.(\d+)\.layer\.2'),
-#              r'decoder.blocks.\1.ffn'),
-#             # encoder has no cross-attn, but rather self-attention+ffn.
-#             (re.compile(r'encoder.block\.(\d+)\.layer\.1'),
-#              r'encoder.blocks.\1.ffn'),
-#             (re.compile(r'\.o\.'), r'.out_proj.'),
-#         ]
-#         stacked_params_mapping = [
-#             # (param_name, shard_name, shard_id)
-#             (".qkv_proj.", ".q.", "q"),
-#             (".qkv_proj.", ".k.", "k"),
-#             (".qkv_proj.", ".v.", "v")
-#         ]
-
-#         for name, loaded_weight in weights:
-#             # No relative position attn bias on cross attention.
-#             if name in self._keys_to_ignore_on_load_unexpected:
-#                 continue
-
-#             # Handle some renaming
-#             for reg in renamed_reg:
-#                 name = re.sub(*reg, name)
-
-#             top_module, _ = name.split('.', 1)
-#             if top_module != 'lm_head':
-#                 name = f"model.{name}"
-
-#             # Split q/k/v layers to unified QKVParallelLinear
-#             for (param_name, weight_name, shard_id) in stacked_params_mapping:
-#                 if weight_name not in name:
-#                     continue
-#                 name = name.replace(weight_name, param_name)
-#                 param = model_params_dict[name]
-#                 weight_loader = param.weight_loader
-#                 weight_loader(param, loaded_weight, shard_id)
-#                 break
-#             else:
-#                 # Not a q/k/v layer.
-#                 param = model_params_dict[name]
-#                 weight_loader = getattr(param, "weight_loader",
-#                                         default_weight_loader)
-#                 weight_loader(param, loaded_weight)
-#             loaded_params.add(name)
-#         return loaded_params
