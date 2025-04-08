b0352932ae1b3c36b91e6ffe96be3706159d99ef
Peiyuan Zhang <a1286225768@gmail.com>
2025-04-08 02:02:00 +0000

    update

--- scripts/inference/v1_inference_hunyuan_STA.sh
+++ scripts/inference/v1_inference_hunyuan_STA.sh
@@ -18,7 +18,7 @@ torchrun --nnodes=1 --nproc_per_node=$num_gpus --master_port 29503 \
     --num_inference_steps 50 \
     --guidance_scale 1 \
     --embedded_cfg_scale 6 \
-    --flow_shift 17 \
+    --flow_shift 7 \
     --prompt_path ./assets/prompt.txt \
     --seed 1024 \
     --output_path outputs_video/ \
