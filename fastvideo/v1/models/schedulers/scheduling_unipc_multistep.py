ed5052230eaf7e962d2293676acb2c069cf1d863
JerryZhou54 <zhouw.jerry2017@outlook.com>
2025-04-11 03:32:05 +0000

    Add ssim tests for I2V

--- fastvideo/v1/models/schedulers/scheduling_unipc_multistep.py
+++ fastvideo/v1/models/schedulers/scheduling_unipc_multistep.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 # Copyright 2024 TSAIL Team and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
