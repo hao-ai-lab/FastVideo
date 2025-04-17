8ba7ea2be84e88ec3eb4ee123e132d60991cd84b
JerryZhou54 <zhouw.jerry2017@outlook.com>
2025-04-17 19:01:43 +0000

    Add more design choices

--- fastvideo/v1/examples/basic.py
+++ fastvideo/v1/examples/basic.py
@@ -5,6 +5,7 @@ prompt2 = "A beautiful woman in a blue dress walking down a street"
 
 
 def main():
+    # Option 1
     # vae_config = VAEConfig.from_pretrained(
     #     "FastVideo/FastHunyuan-Diffusers",
     #     load_encoder_only=True, 
@@ -25,6 +26,49 @@ def main():
         distributed_executor_backend="mp",
         # vae_config=vae_config,
         # dit_config=dit_config,
+    )
+
+    # Option 2
+    generator = VideoGenerator.from_pretrained(
+        "FastVideo/FastHunyuan-Diffusers",
+        num_gpus=4,
+        num_inference_steps=2,
+        distributed_executor_backend="mp",
+        # component_configs={
+        #     "vae": {
+        #         "load_encoder_only": True,
+        #         "use_temporal_tiling": True
+        #     },
+        #     "dit": {
+        #         "use_teacache": True,
+        #         "teacache_kwargs": {"num_steps": 10, "rel_l1_thresh": 0.5},
+        #         "quant_config": fp8_quant_config
+        #     }
+        # }
+    )
+
+    # Option 3
+    VideoGenerator.dump_default_config("FastVideo/FastHunyuan-Diffusers", "config.yaml")
+    # This generates a yaml file that looks like:
+    # vae_config:
+    #     load_encoder_only: true
+    #     use_temporal_tiling: false
+
+    # dit_config:
+    #     use_teacache: true
+    #     teacache_kwargs:
+    #         num_steps: 8
+    #         rel_l1_thresh: 0.5
+    #     quant_config: null
+
+    # num_gpus: 1
+    # num_inference_steps: 50
+    # distributed_executor_backend: "mp"
+
+    # Then the users can edit the yaml file however they want
+    generator = VideoGenerator.from_pretrained(
+        "FastVideo/FastHunyuan-Diffusers", 
+        config_path="config.yaml"
     )
 
     # Generate videos with the same simple API, regardless of GPU count
