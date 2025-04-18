running ver of stepvideo v1 (wrong result)

fastvideo/v1/models/dits/stepvideo.py
@@ -26,7 +26,7 @@ from fastvideo.v1.models.dits.base import BaseDiT
 # from fastvideo.models.stepvideo.modules.normalization import AdaLayerNormSingle, PixArtAlphaTextProjection
 # from fastvideo.models.stepvideo.parallel import parallel_forward
 # from fastvideo.models.stepvideo.utils import with_empty_init
-from fastvideo.v1.models.dits.temp import StepVideoTransformerBlock, AdaLayerNormSingle, PixArtAlphaTextProjection, parallel_forward, with_empty_init
+from fastvideo.v1.models.dits.temp import StepVideoTransformerBlock, AdaLayerNormSingle, PixArtAlphaTextProjection, with_empty_init
 
 from fastvideo.v1.platforms import _Backend
 
@@ -56,7 +56,7 @@ class StepVideoModel(BaseDiT):
         norm_eps: float = 1e-6,
         use_additional_conditions: Optional[bool] = False,
         caption_channels: Optional[int] | list | tuple = [6144, 1024],
-        attention_type: Optional[str] = "parallel",
+        attention_type: Optional[str] = "torch",
     ):
         super().__init__()
         # Instead of using self.config, assign each parameter as an instance variable.
@@ -129,7 +129,6 @@ class StepVideoModel(BaseDiT):
             mask[i, :, :kv_len] = 1
         return encoder_hidden_states, mask
 
-    @parallel_forward
     def block_forward(self,
                       hidden_states,
                       encoder_hidden_states=None,
@@ -186,20 +185,25 @@ class StepVideoModel(BaseDiT):
             added_cond_kwargs = {}
 
         t_expand, embedded_timestep = self.adaln_single(t_expand, added_cond_kwargs=added_cond_kwargs)
-
-        encoder_hidden_states = self.caption_projection(self.caption_norm(encoder_hidden_states))
-
-        if encoder_hidden_states_2 is not None and hasattr(self, 'clip_projection'):
-            clip_embedding = self.clip_projection(encoder_hidden_states_2)
-            encoder_hidden_states = torch.cat([clip_embedding, encoder_hidden_states], dim=1)
+        print(f">>> encoder hidden states in {encoder_hidden_states.shape}")
         
-        print("  bsz=", bsz, "frame=", frame, "len_frame=", len_frame," hidden_states.shape[0]=", hidden_states.shape[0])
-
+        encoder_hidden_states = self.caption_projection(self.caption_norm(encoder_hidden_states))
+        print(f">>> encoder hidden states after caption_projection{encoder_hidden_states.shape}")
+        
+        # if encoder_hidden_states_2 is not None and hasattr(self, 'clip_projection'):
+        #     clip_embedding = self.clip_projection(encoder_hidden_states_2)
+        #     print(f">>> encoder hidden states 2 {encoder_hidden_states_2.shape}")
+        #     print(f">>> clip_embedding {clip_embedding.shape}")
+        #     encoder_hidden_states = torch.cat([clip_embedding, encoder_hidden_states], dim=1)
+        #     print(f">>> encoder hidden states after cat {encoder_hidden_states.shape}")
+        # print("  bsz=", bsz, "frame=", frame, "len_frame=", len_frame," hidden_states.shape[0]=", hidden_states.shape[0])
+        
+        print(">>> before block_forward:", hidden_states.shape)
         hidden_states = rearrange(hidden_states, '(b f) l d->  b (f l) d', b=bsz, f=frame, l=len_frame).contiguous()
         encoder_hidden_states, attn_mask = self.prepare_attn_mask(encoder_attention_mask,
                                                                   encoder_hidden_states,
                                                                   q_seqlen=frame * len_frame)
-
+        print(">>> before block_forward:", hidden_states.shape)
         hidden_states = self.block_forward(hidden_states,
                                            encoder_hidden_states,
                                            t_expand=t_expand,
@@ -207,7 +211,8 @@ class StepVideoModel(BaseDiT):
                                            attn_mask=attn_mask,
                                            parallel=self.parallel,
                                            mask_strategy=mask_strategy)
-
+        print(">>> after block_forward:", hidden_states.shape)
+        
         hidden_states = rearrange(hidden_states, 'b (f l) d -> (b f) l d', b=bsz, f=frame, l=len_frame)
 
         embedded_timestep = repeat(embedded_timestep, 'b d -> (b f) d', f=frame).contiguous()
@@ -225,8 +230,7 @@ class StepVideoModel(BaseDiT):
         hidden_states = rearrange(hidden_states, 'n h w p q c -> n c h p w q')
         output = hidden_states.reshape(shape=(-1, self.out_channels, height * self.patch_size, width * self.patch_size))
 
-        output = rearrange(output, '(b f) c h w -> b f c h w', f=frame)
-
-        if return_dict:
-            return {'x': output}
+        output = rearrange(output, '(b f) c h w -> b c f h w', f=frame)
+        # if return_dict:
+        #     return {'x': output}
         return output

fastvideo/v1/models/dits/temp.py
@@ -26,7 +26,7 @@ except ImportError:
 
 from fastvideo.utils.communications import all_to_all_4D, all_gather
 from fastvideo.utils.parallel_states import get_sequence_parallel_state, nccl_info
-
+print(">>> temp.py loaded, nccl_info.sp_size =", nccl_info.sp_size)
 class RoPE1D:
 
     def __init__(self, freq=1e4, F0=1.0, scaling_factor=1.0):
@@ -178,8 +178,8 @@ class Attention(nn.Module):
     def attn_processor(self, attn_type):
         if attn_type == 'torch':
             return self.torch_attn_func
-        elif attn_type == 'parallel':
-            return self.parallel_attn_func
+        # elif attn_type == 'parallel':
+        #     return self.parallel_attn_func
         else:
             raise Exception('Not supported attention type...')
 
@@ -224,33 +224,33 @@ class Attention(nn.Module):
         x = rearrange(x, 'b h s d -> b s h d')
         return x
 
-    def parallel_attn_func(self, q, k, v, causal=False, mask_strategy=None, **kwargs):
-        if get_sequence_parallel_state():
-            q = all_to_all_4D(q, scatter_dim=2, gather_dim=1)
-            k = all_to_all_4D(k, scatter_dim=2, gather_dim=1)
-            v = all_to_all_4D(v, scatter_dim=2, gather_dim=1)
+    # def parallel_attn_func(self, q, k, v, causal=False, mask_strategy=None, **kwargs):
+    #     if get_sequence_parallel_state():
+    #         q = all_to_all_4D(q, scatter_dim=2, gather_dim=1)
+    #         k = all_to_all_4D(k, scatter_dim=2, gather_dim=1)
+    #         v = all_to_all_4D(v, scatter_dim=2, gather_dim=1)
 
-        if mask_strategy is not None and mask_strategy[0] is not None:
-            q = self.tile(q, nccl_info.sp_size).transpose(1, 2).contiguous()
-            k = self.tile(k, nccl_info.sp_size).transpose(1, 2).contiguous()
-            v = self.tile(v, nccl_info.sp_size).transpose(1, 2).contiguous()
+    #     if mask_strategy is not None and mask_strategy[0] is not None:
+    #         q = self.tile(q, nccl_info.sp_size).transpose(1, 2).contiguous()
+    #         k = self.tile(k, nccl_info.sp_size).transpose(1, 2).contiguous()
+    #         v = self.tile(v, nccl_info.sp_size).transpose(1, 2).contiguous()
 
-            head_num = q.size(1)  # 48 // sp_size
-            current_rank = nccl_info.rank_within_group
+    #         head_num = q.size(1)  # 48 // sp_size
+    #         current_rank = nccl_info.rank_within_group
 
-            start_head = current_rank * head_num
-            windows = [mask_strategy[head_idx + start_head] for head_idx in range(head_num)]
+    #         start_head = current_rank * head_num
+    #         windows = [mask_strategy[head_idx + start_head] for head_idx in range(head_num)]
 
-            x = sliding_tile_attention(q, k, v, windows, 0, False).transpose(1, 2).contiguous()
-            x = self.untile(x, nccl_info.sp_size)
-        else:
-            x = flash_attn_func(q, k, v, dropout_p=0.0, softmax_scale=None, causal=False)
+    #         x = sliding_tile_attention(q, k, v, windows, 0, False).transpose(1, 2).contiguous()
+    #         x = self.untile(x, nccl_info.sp_size)
+    #     else:
+    #         x = flash_attn_func(q, k, v, dropout_p=0.0, softmax_scale=None, causal=False)
 
-        if get_sequence_parallel_state():
-            x = all_to_all_4D(x, scatter_dim=1, gather_dim=2)
+    #     if get_sequence_parallel_state():
+    #         x = all_to_all_4D(x, scatter_dim=1, gather_dim=2)
 
-        x = x.to(q.dtype)
-        return x
+    #     x = x.to(q.dtype)
+    #     return x
 
 class SelfAttention(Attention):
 
@@ -273,7 +273,7 @@ class SelfAttention(Attention):
             self.rope_ch_split = [64, 32, 32]
 
         self.core_attention = self.attn_processor(attn_type=attn_type)
-        self.parallel = attn_type == 'parallel'
+        self.parallel = attn_type == 'torch'
 
     def apply_rope3d(self, x, fhw_positions, rope_ch_split, parallel=True):
         x = self.rope_3d(x, fhw_positions, rope_ch_split, parallel)
@@ -325,6 +325,7 @@ class CrossAttention(Attention):
         self.core_attention = self.attn_processor(attn_type=attn_type)
 
     def forward(self, x: torch.Tensor, encoder_hidden_states: torch.Tensor, attn_mask=None):
+        
         xq = self.wq(x)
         xq = xq.view(*xq.shape[:-1], self.n_heads, self.head_dim)
 
@@ -446,7 +447,7 @@ class StepVideoTransformerBlock(nn.Module):
                  norm_eps: float = 1e-5,
                  ff_inner_dim: Optional[int] = None,
                  ff_bias: bool = False,
-                 attention_type: str = 'parallel'):
+                 attention_type: str = 'torch'):
         super().__init__()
         self.dim = dim
         self.norm1 = nn.LayerNorm(dim, eps=norm_eps)
@@ -472,6 +473,7 @@ class StepVideoTransformerBlock(nn.Module):
                 attn_mask=None,
                 rope_positions: list = None,
                 mask_strategy=None) -> torch.Tensor:
+
         shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = (torch.clone(chunk) for chunk in (
             self.scale_shift_table[None] + t_expand.reshape(-1, 6, self.dim)).chunk(6, dim=1))
 
@@ -743,21 +745,22 @@ class PixArtAlphaTextProjection(nn.Module):
 
 
 
-def parallel_forward(fn_):
+# def parallel_forward(fn_):
 
-    def wrapTheFunction(_, hidden_states, *args, **kwargs):
-        if kwargs['parallel']:
-            hidden_states = torch.chunk(hidden_states, nccl_info.sp_size, dim=-2)[nccl_info.rank_within_group]
-            kwargs['attn_mask'] = torch.chunk(kwargs['attn_mask'], nccl_info.sp_size,
-                                              dim=-2)[nccl_info.rank_within_group]
-        output = fn_(_, hidden_states, *args, **kwargs)
+#     def wrapTheFunction(_, hidden_states, *args, **kwargs):
+#         print(f">>> [parallel_forward] nccl_info.sp_size={nccl_info.sp_size}, rank={nccl_info.rank_within_group}")
+#         if kwargs['parallel']:
+#             hidden_states = torch.chunk(hidden_states, nccl_info.sp_size, dim=-2)[nccl_info.rank_within_group]
+#             kwargs['attn_mask'] = torch.chunk(kwargs['attn_mask'], nccl_info.sp_size,
+#                                               dim=-2)[nccl_info.rank_within_group]
+#         output = fn_(_, hidden_states, *args, **kwargs)
 
-        if kwargs['parallel']:
-            output = all_gather(output.contiguous(), dim=-2)
+#         if kwargs['parallel']:
+#             output = all_gather(output.contiguous(), dim=-2)
 
-        return output
+#         return output
 
-    return wrapTheFunction
+#     return wrapTheFunction
 class EmptyInitOnDevice(torch.overrides.TorchFunctionMode):
 
     def __init__(self, device=None):

fastvideo/v1/models/vaes/stepvideovae.py
@@ -16,7 +16,7 @@ from torch import nn
 from torch.nn import functional as F
 
 from fastvideo.models.stepvideo.utils import with_empty_init
-
+from fastvideo.v1.models.vaes.common import ParallelTiledVAE
 
 def base_group_norm(x, norm_layer, act_silu=False, channel_last=False):
     if hasattr(base_group_norm, 'spatial') and base_group_norm.spatial:
@@ -854,7 +854,7 @@ class DiagonalGaussianDistribution(object):
             return x
 
 
-class AutoencoderKLStepvideo(nn.Module):
+class AutoencoderKLStepvideo(nn.Module, ParallelTiledVAE):
 
     @with_empty_init
     def __init__(
@@ -896,7 +896,27 @@ class AutoencoderKLStepvideo(nn.Module):
         self.convert_channel_last()
 
         self.world_size = world_size
+        
+        # ────── tiling flags ──────
+        self.use_tiling           = False
+        self.se_temporal_tiling  = False
+        self.use_parallel_tiling  = False
 
+        # ──── dummy tile sizes (must cover full input) ────
+        self.tile_sample_min_height     = 128   # or whatever H×W your frames are
+        self.tile_sample_min_width      = 128
+        self.tile_sample_min_num_frames = 17
+        self.tile_sample_stride_height     = 128
+        self.tile_sample_stride_width      = 128
+        self.tile_sample_stride_num_frames = 17
+
+        # ──── compression ratios from VideoEncoder ────
+        self.spatial_compression_ratio  = 8
+        self.temporal_compression_ratio = 4
+
+        # ───── scale factor from your server ─────
+        self.scaling_factor = 1.0
+        
     def init_from_ckpt(self, model_path):
         from safetensors import safe_open
         p = {}
@@ -915,7 +935,7 @@ class AutoencoderKLStepvideo(nn.Module):
         #Conv2d NCHW->NHWC
         pass
 
-    def naive_encode(self, x, is_init_image=True):
+    def _encode(self, x, is_init_image=True):
         b, len, c, h, w = x.size()
         x = rearrange(x, 'b l c h w -> b c l h w').contiguous()
         z = self.encoder(x, len, True)  # 下采样[1, 4, 8, 16, 16]
@@ -926,13 +946,13 @@ class AutoencoderKLStepvideo(nn.Module):
         # b (nc cf) c h w -> (b nc) cf c h w -> encode -> (b nc) cf c h w -> b (nc cf) c h w
         chunks = list(x.split(self.frame_len, dim=1))
         for i in range(len(chunks)):
-            chunks[i] = self.naive_encode(chunks[i], True)
+            chunks[i] = self._encode(chunks[i], True)
         z = torch.cat(chunks, dim=1)
 
         posterior = DiagonalGaussianDistribution(z)
         return posterior.sample()
 
-    def decode_naive(self, z, is_init=True):
+    def _decode(self, z, is_init=True):
         z = z.to(next(self.decoder.parameters()).dtype)
         dec = self.decoder(z, is_init)
         return dec
@@ -952,7 +972,7 @@ class AutoencoderKLStepvideo(nn.Module):
             chunks = chunks_
 
         for i in range(len(chunks)):
-            chunks[i] = self.decode_naive(chunks[i], True).permute(0, 2, 1, 3, 4)
+            chunks[i] = self._decode(chunks[i], True).permute(0, 2, 1, 3, 4)
         x = torch.cat(chunks, dim=1)
 
         if self.world_size > 1: