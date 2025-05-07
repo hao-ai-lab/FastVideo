af882ec6dbc33117ddcb11ff829f43da737ef2ce
Zihang-He <z6he@ucsd.edu>
2025-05-07 18:26:36 +0000

    fixed timesteps scale ssim test

--- fastvideo/v1/fastvideo_args.py
+++ fastvideo/v1/fastvideo_args.py
@@ -89,7 +89,7 @@ class FastVideoArgs:
         str] = "超高清、HDR 视频、环境光、杜比全景声、画面稳定、流畅动作、逼真的细节、专业级构图、超现实主义、自然、生动、超细节、清晰。"
     neg_magic: Optional[
         str] = "画面暗、低分辨率、不良手、文本、缺少手指、多余的手指、裁剪、低质量、颗粒状、签名、水印、用户名、模糊。"
-    timesteps_scale: Optional[bool] = True
+    timesteps_scale: Optional[bool] = None
 
     # Logging
     log_level: str = "info"
@@ -280,9 +280,9 @@ class FastVideoArgs:
         )
         parser.add_argument(
             "--timesteps_scale",
-            type=str,
+            type=bool,
             default=FastVideoArgs.timesteps_scale,
-            help="Negative magic prompt for sampling",
+            help="Bool for applying scheduler scale in set_timesteps",
         )
 
         # Logging
