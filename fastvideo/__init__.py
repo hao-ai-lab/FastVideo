55edf1ee79f412e6ef4997401f4848d6d6f62c18
Will Lin <wlsaidhi@gmail.com>
2025-05-10 16:22:59 -0700

    update

--- fastvideo/__init__.py
+++ fastvideo/__init__.py
@@ -1,5 +1,5 @@
-from fastvideo.v1.configs.pipelines.base import PipelineConfig
-from fastvideo.v1.configs.sample.base import SamplingParam
+from fastvideo.v1.configs.pipelines import PipelineConfig
+from fastvideo.v1.configs.sample import SamplingParam
 from fastvideo.v1.entrypoints.video_generator import VideoGenerator
 
 __all__ = ["VideoGenerator", "PipelineConfig", "SamplingParam"]
