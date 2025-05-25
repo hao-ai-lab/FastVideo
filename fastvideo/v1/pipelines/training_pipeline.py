d6365373b418699a7269827f6187e70a10a35828
Zihang-He <z6he@ucsd.edu>
2025-05-25 23:43:45 +0000

    added gradient clipping

--- fastvideo/v1/pipelines/training_pipeline.py
+++ fastvideo/v1/pipelines/training_pipeline.py
@@ -30,7 +30,7 @@ from fastvideo.v1.logger import init_logger
 from fastvideo.v1.pipelines import ComposedPipelineBase
 from fastvideo.v1.pipelines.pipeline_batch_info import ForwardBatch
 from fastvideo.v1.pipelines.wan.wan_pipeline import WanValidationPipeline
-
+from fastvideo.v1.pipelines.training_utils import _clip_grad_norm_while_handling_failing_dtensor_cases
 logger = init_logger(__name__)
 
 # Manual gradient checking flag - set to True to enable gradient verification
@@ -683,17 +683,21 @@ class WanTrainingPipeline(TrainingPipeline):
             sp_group = get_sp_group()
             sp_group.all_reduce(avg_loss, op=torch.distributed.ReduceOp.AVG)
             total_loss += avg_loss.item()
-
-        # TODO(will): clip grad norm
-        # grad_norm = transformer.clip_grad_norm_(max_grad_norm)
+        
+        model_parts = [self.transformer]
+        grad_norm = _clip_grad_norm_while_handling_failing_dtensor_cases(
+            [p for m in model_parts for p in m.parameters()],
+            max_grad_norm,
+            foreach=None,
+        )
+        
         optimizer.step()
         print('device after optimizer step',
               next(transformer.named_parameters())[1].device)
         lr_scheduler.step()
         print('device after scheduler step',
               next(transformer.named_parameters())[1].device)
-        return total_loss, 0.0
-        # return total_loss, grad_norm.item()
+        return total_loss, grad_norm.item()
 
     def forward(
         self,
