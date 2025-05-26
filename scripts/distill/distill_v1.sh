30db272ac6ddf122ae13597020ec1c9c794cd772
Jinzhe Pan <eigensystem1318@gmail.com>
2025-05-26 17:07:50 +0800

    [WIP][Feat] distillation

--- scripts/distill/distill_v1.sh
+++ scripts/distill/distill_v1.sh
@@ -1,54 +1,47 @@
 export WANDB_BASE_URL="https://api.wandb.ai"
 export WANDB_MODE=online
-# export FASTVIDEO_ATTENTION_BACKEND=TORCH_SDPA
 
 DATA_DIR=data/HD-Mixkit-Finetune-Wan/combined_parquet_dataset
 VALIDATION_DIR=data/HD-Mixkit-Finetune-Wan/validation_parquet_dataset
-NUM_GPUS=1
-# export CUDA_VISIBLE_DEVICES=4,5
+num_gpus=1
 # IP=[MASTER NODE IP]
 
-CHECKPOINT_PATH="$DATA_DIR/outputs/wan_finetune/checkpoint-5"
-
 # If you do not have 32 GPUs and to fit in memory, you can: 1. increase sp_size. 2. reduce num_latent_t
-torchrun --nnodes 1 --nproc_per_node $NUM_GPUS\
-    fastvideo/v1/training/wan_training_pipeline.py\
-    --model_path Wan-AI/Wan2.1-T2V-1.3B-Diffusers \
-    --mode training\
+    # --gradient_checkpointing\
+    # --pretrained_model_name_or_path hunyuanvideo-community/HunyuanVideo \
+    # --pretrained_model_name_or_path Wan-AI/Wan2.1-T2V-1.3B-Diffusers \
+torchrun --nnodes 1 --nproc_per_node $num_gpus\
+    fastvideo/v1/pipelines/distillation_pipeline.py\
+    --mode distill\
     --pretrained_model_name_or_path Wan-AI/Wan2.1-T2V-1.3B-Diffusers \
-    --cache_dir "/home/ray/.cache"\
+    --cache_dir "/home/test/.cache"\
     --data_path "$DATA_DIR"\
     --validation_prompt_dir "$VALIDATION_DIR"\
-    --train_batch_size=1\
-    --num_latent_t 4 \
-    --sp_size $NUM_GPUS \
-    --tp_size $NUM_GPUS \
+    --train_batch_size=1 \
+    --num_latent_t 1 \
+    --sp_size $num_gpus \
     --train_sp_batch_size 1\
-    --dataloader_num_workers 5\
+    --dataloader_num_workers $num_gpus\
     --gradient_accumulation_steps=1\
-    --max_train_steps=120 \
+    --max_train_steps=320\
     --learning_rate=1e-6\
     --mixed_precision="bf16"\
-    --checkpointing_steps=50 \
+    --checkpointing_steps=64\
     --validation_steps 20\
     --validation_sampling_steps "2,4,8" \
-    --log_validation \
     --checkpoints_total_limit 3\
     --allow_tf32\
     --ema_start_step 0\
     --cfg 0.0\
-    --output_dir="$DATA_DIR/outputs/wan_finetune"\
-    --tracker_project_name wan_finetune \
-    --num_height 480 \
-    --num_width 832 \
-    --num_frames  81 \
-    --shift 3 \
+    --log_validation\
+    --output_dir="$DATA_DIR/outputs/hy_phase1_shift17_bs_16_HD"\
+    --tracker_project_name Hunyuan_Distill \
+    --num_height 720 \
+    --num_width 1280 \
+    --num_frames  125 \
+    --shift 17 \
     --validation_guidance_scale "1.0" \
     --num_euler_timesteps 50 \
     --multi_phased_distill_schedule "4000-1" \
-    --weight_decay 0.01 \
     --not_apply_cfg_solver \
-    --master_weight_type "fp32" \
-    --max_grad_norm 1.0 \
-
-# --resume_from_checkpoint "$CHECKPOINT_PATH"
+    --master_weight_type "bf16"
