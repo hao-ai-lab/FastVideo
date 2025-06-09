6b89e1aba71866813148881c2b679ccd9512ac95
“BrianChen1129” <yongqich@umich.edu>
2025-06-09 22:39:20 +0000

    update STA mode default value

--- fastvideo/v1/fastvideo_args.py
+++ fastvideo/v1/fastvideo_args.py
@@ -85,8 +85,8 @@ class FastVideoArgs:
     postprocess_text_funcs: Tuple[Callable[[Any], Any], ...] = field(
         default_factory=lambda: (postprocess_text, ))
 
-    # STA (Spatial-Temporal Attention) parameters
-    STA_mode: str = "STA_inference"
+    # STA parameters
+    STA_mode: str = None
     skip_time_steps: int = 15
     # LoRA parameters
     lora_path: Optional[str] = None
@@ -282,7 +282,7 @@ class FastVideoArgs:
             type=str,
             default=FastVideoArgs.STA_mode,
             choices=[
-                "STA_inference", "STA_searching", "STA_tuning", "STA_tuning_cfg"
+                "STA_inference", "STA_searching", "STA_tuning", "STA_tuning_cfg", None
             ],
             help="STA mode",
         )
