f07bc09a7eaa00f95ac0b28829d6fd52d438b57f
Edenzzzz <wtan45@wisc.edu>
2025-06-26 20:17:36 -0700

    fix

--- fastvideo/v1/models/loader/fsdp_load.py
+++ fastvideo/v1/models/loader/fsdp_load.py
@@ -105,7 +105,7 @@ def maybe_load_fsdp_model(
                 fsdp_shard_conditions=model._fsdp_shard_conditions)
 
     weight_iterator = safetensors_weights_iterator(weight_dir_list,
-                                                   async_broadcast=True)
+                                                   async_broadcast=False)
     param_names_mapping_fn = get_param_names_mapping(model._param_names_mapping)
     load_model_from_full_model_state_dict(
         model,
--- fastvideo/v1/models/loader/weight_utils.py
+++ fastvideo/v1/models/loader/weight_utils.py
@@ -147,26 +147,21 @@ def safetensors_weights_iterator(
     ):
         with safe_open(st_file, framework="pt", device=device) as f:
             for name in f.keys():  # noqa: SIM118
-                if to_cpu:
+                if local_rank == 0:
                     param = f.get_tensor(name)
                 else:
-                    if local_rank == 0:
-                        param = f.get_tensor(name)
-                    else:
-                        shape = f.get_slice(name).get_shape()
-                        param = torch.empty(shape, device=device)
-                    # broadcast to local ranks
-                    # TODO(Wenxuan): scatter instead of broadcast
-                    group = get_node_group().device_group
-                    if async_broadcast:
-                        handle = dist.broadcast(param,
-                                                src=dist.get_global_rank(
-                                                    group, 0),
-                                                async_op=True)
-                        handles.append(handle)
-                    else:
-                        dist.broadcast(param,
-                                       src=dist.get_global_rank(group, 0))
+                    shape = f.get_slice(name).get_shape()
+                    param = torch.empty(shape, device=device)
+                # broadcast to local ranks
+                # TODO(Wenxuan): scatter instead of broadcast
+                group = get_node_group().device_group
+                if async_broadcast:
+                    handle = dist.broadcast(param,
+                                            src=dist.get_global_rank(group, 0),
+                                            async_op=True)
+                    handles.append(handle)
+                else:
+                    dist.broadcast(param, src=dist.get_global_rank(group, 0))
                 yield name, param
 
         if async_broadcast:
