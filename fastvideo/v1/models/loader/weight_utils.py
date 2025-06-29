69ebe98bb6f74ea57977c35be026b6cde5508848
Edenzzzz <wtan45@wisc.edu>
2025-06-28 18:32:13 -0700

    fix

--- fastvideo/v1/models/loader/weight_utils.py
+++ fastvideo/v1/models/loader/weight_utils.py
@@ -152,7 +152,7 @@ def safetensors_weights_iterator(
                 if to_cpu:
                     param = f.get_tensor(name)
                 else:
-                    if local_rank == 0 or use_dist_load:
+                    if local_rank == 0 or not use_dist_load:
                         param = f.get_tensor(name)
                     else:
                         shape = f.get_slice(name).get_shape()
