aa6d4c55629ad8c44e8aa786bf10ea56ab04cccf
Matthew Noto <notomatthew31@gmail.com>
2025-07-07 03:12:14 -0700

    clean up

--- fastvideo/v1/pipelines/stages/text_encoding.py
+++ fastvideo/v1/pipelines/stages/text_encoding.py
@@ -115,9 +115,6 @@ class TextEncodingStage(PipelineStage):
                     batch.negative_attention_mask.append(
                         negative_attention_mask)
 
-            # if fastvideo_args.text_encoder_offload:
-            #     text_encoder.to('cpu')
-
             # deallocate text encoder and tokenizer if on mps
             if torch.backends.mps.is_available():
                 del text_encoder
