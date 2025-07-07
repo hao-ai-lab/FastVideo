4f1ee9c0867936c0d92690cdf9eeb46ad8bd3757
Matthew Noto <notomatthew31@gmail.com>
2025-07-07 01:17:20 -0700

    fix offloading for mps

--- fastvideo/v1/distributed/device_communicators/base_device_communicator.py
+++ fastvideo/v1/distributed/device_communicators/base_device_communicator.py
@@ -187,8 +187,7 @@ class DeviceCommunicatorBase:
                  device: torch.device | None = None,
                  device_group: ProcessGroup | None = None,
                  unique_name: str = ""):
-        # self.device = device or torch.device("cpu")
-        self.device = device
+        self.device = device or torch.device("cpu")
         self.cpu_group = cpu_group
         self.device_group = device_group
         self.unique_name = unique_name
