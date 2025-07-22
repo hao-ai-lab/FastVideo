3829810f62b10a98e957f86bf2b18f917e03b025
Edenzzzz <wtan45@wisc.edu>
2025-07-21 21:15:09 -0700

    fix

--- examples/training/finetune/wan_t2v_1_3b/crush_smol/finetune_t2v_lora.sh
+++ examples/training/finetune/wan_t2v_1_3b/crush_smol/finetune_t2v_lora.sh
@@ -19,7 +19,7 @@ training_args=(
   --train_batch_size 1
   --train_sp_batch_size 1
   --gradient_accumulation_steps 8
-  --num_latent_t 8 
+  --num_latent_t 20
   --num_height 480
   --num_width 832
   --num_frames 77
