c105204c85c0a6fa48d07a911594d6f64e8aac72
“BrianChen1129” <yongqich@umich.edu>
2025-07-25 22:31:46 +0000

    add prompt_txt support

---
+++ scripts/inference/v1_inference_wan_dmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+num_gpus=1
+export FASTVIDEO_ATTENTION_BACKEND=VIDEO_SPARSE_ATTN
+export MODEL_BASE=FastVideo/FastWan2.1-T2V-1.3B-Diffusers
+# export MODEL_BASE=hunyuanvideo-community/HunyuanVideo
+# You can either use --prompt or --prompt-txt, but not both.
+fastvideo generate \
+    --model-path $MODEL_BASE \
+    --sp-size $num_gpus \
+    --tp-size 1 \
+    --num-gpus $num_gpus \
+    --height 448 \
+    --width 832 \
+    --num-frames 61 \
+    --num-inference-steps 3 \
+    --fps 16 \
+    --prompt-txt assets/prompt.txt \
+    --negative-prompt "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards" \
+    --seed 1024 \
+    --output-path outputs_video_dmd/ \
+    --VSA-sparsity 0.8 \
+    --dmd-denoising-steps "1000,757,522"
