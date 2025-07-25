71a561bfc9eae3caf9d32944823cd127d6863d07
“BrianChen1129” <yongqich@umich.edu>
2025-07-25 22:23:54 +0000

    add prompt_txt support

--- scripts/inference/v1_inference_wan.sh
+++ scripts/inference/v1_inference_wan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-num_gpus=2
+num_gpus=1
 export FASTVIDEO_ATTENTION_BACKEND=
 export MODEL_BASE=Wan-AI/Wan2.1-T2V-1.3B-Diffusers
 # export MODEL_BASE=hunyuanvideo-community/HunyuanVideo
+# You can either use --prompt or --prompt-txt, but not both.
 fastvideo generate \
     --model-path $MODEL_BASE \
     --sp-size $num_gpus \
@@ -14,8 +15,9 @@ fastvideo generate \
     --num-frames 77 \
     --num-inference-steps 50 \
     --fps 16 \
-    --guidance-scale 3.0 \
-    --prompt "A beautiful woman in a red dress walking down a street" \
+    --guidance-scale 6.0 \
+    --flow-shift 8.0 \
+    --prompt-txt assets/prompt.txt \
     --negative-prompt "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards" \
     --seed 1024 \
     --output-path outputs_video/
