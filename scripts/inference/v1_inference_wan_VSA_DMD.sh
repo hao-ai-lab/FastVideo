e2aae76a07bc63d165c4eb4d45aa3ed86656c436
“BrianChen1129” <yongqich@umich.edu>
2025-08-04 18:50:20 +0000

    update

--- scripts/inference/v1_inference_wan_VSA_DMD.sh
+++ scripts/inference/v1_inference_wan_VSA_DMD.sh
@@ -79,3 +79,26 @@ fastvideo generate \
     --VSA-sparsity 0.9 \
     --dmd-denoising-steps "1000,757,522" \
     --enable_torch_compile 
+
+# 720P 5B
+num_gpus=1
+export FASTVIDEO_ATTENTION_BACKEND=FLASH_ATTN
+export MODEL_BASE=FastVideo/FastWan2.2-TI2V-5B-FullAttn-Diffusers
+# export MODEL_BASE=hunyuanvideo-community/HunyuanVideo
+# You can either use --prompt or --prompt-txt, but not both.
+fastvideo generate \
+    --model-path $MODEL_BASE \
+    --sp-size $num_gpus \
+    --tp-size 1 \
+    --num-gpus $num_gpus \
+    --height 704 \
+    --width 1280 \
+    --num-frames 104 \
+    --num-inference-steps 3 \
+    --fps 24 \
+    --prompt-txt assets/prompt.txt \
+    --negative-prompt "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards" \
+    --seed 1024 \
+    --output-path outputs_video_dmd_5B_720P/ \
+    --dmd-denoising-steps "1000,757,522" \
+    --enable_torch_compile 
