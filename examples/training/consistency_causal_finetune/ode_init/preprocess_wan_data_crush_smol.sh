7b7a895e77ae00b6dfdd0666845d85bc5c5690f8
SolitaryThinker <wlsaidhi@gmail.com>
2025-09-06 02:19:18 +0000

    checkpoint

--- examples/training/consistency_causal_finetune/ode_init/preprocess_wan_data_crush_smol.sh
+++ examples/training/consistency_causal_finetune/ode_init/preprocess_wan_data_crush_smol.sh
@@ -3,8 +3,8 @@
 GPU_NUM=1 # 2,4,8
 MODEL_PATH="Wan-AI/Wan2.1-T2V-1.3B-Diffusers"
 MODEL_TYPE="wan"
-DATA_MERGE_PATH="data/crush-smol/merge.txt"
-OUTPUT_DIR="data/crush-smol_processed_t2v_1_3b_ode_init_5/"
+DATA_MERGE_PATH="$(dirname "$0")/crush_smol_prompts.txt"
+OUTPUT_DIR="data/crush-smol_processed_t2v_1_3b_ode_init/"
 
 torchrun --nproc_per_node=$GPU_NUM \
     fastvideo/pipelines/preprocess/v1_preprocess.py \
@@ -18,7 +18,7 @@ torchrun --nproc_per_node=$GPU_NUM \
     --dataloader_num_workers 0 \
     --output_dir=$OUTPUT_DIR \
     --train_fps 16 \
-    --samples_per_file 5 \
-    --flush_frequency 5 \
+    --samples_per_file 8 \
+    --flush_frequency 8 \
     --video_length_tolerance_range 5 \
     --preprocess_task "ode_trajectory" 
