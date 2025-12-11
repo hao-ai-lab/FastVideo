e5cb1efb09de3e48de7ed4da1570210b2912b806
JerryZhou54 <zhouw.jerry2017@outlook.com>
2025-12-11 00:41:57 +0000

    Added hy1.5 vae with 0 numerical diff with diffusers

--- fastvideo/configs/models/vaes/__init__.py
+++ fastvideo/configs/models/vaes/__init__.py
@@ -1,5 +1,6 @@
 from fastvideo.configs.models.vaes.cosmosvae import CosmosVAEConfig
 from fastvideo.configs.models.vaes.hunyuanvae import HunyuanVAEConfig
+from fastvideo.configs.models.vaes.hunyuan15vae import Hunyuan15VAEConfig
 from fastvideo.configs.models.vaes.stepvideovae import StepVideoVAEConfig
 from fastvideo.configs.models.vaes.wanvae import WanVAEConfig
 
@@ -8,4 +9,5 @@ __all__ = [
     "WanVAEConfig",
     "StepVideoVAEConfig",
     "CosmosVAEConfig",
+    "Hunyuan15VAEConfig",
 ]
