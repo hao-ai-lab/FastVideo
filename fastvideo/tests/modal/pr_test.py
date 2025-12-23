e932bf80cacecdf6ec2b48996be37b7a8a592a2d
alexzms <3036648523@qq.com>
2025-12-23 05:11:08 +0000

    remove ssim in this pr

--- fastvideo/tests/modal/pr_test.py
+++ fastvideo/tests/modal/pr_test.py
@@ -78,7 +78,7 @@ def run_transformer_tests():
 @app.function(
     gpu="L40S:4", 
     image=image, 
-    timeout=5400, 
+    timeout=2700, 
     secrets=[modal.Secret.from_dict({"HF_API_KEY": os.environ.get("HF_API_KEY", "")})],
     volumes={"/root/data": model_vol} 
 )
