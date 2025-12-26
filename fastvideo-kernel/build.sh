403054dfa5d67d0400cbf0fda1a5f3236f7f72fa
Will Lin <wlsaidhi@gmail.com>
2025-12-26 11:54:00 -0800

    release

--- fastvideo-kernel/build.sh
+++ fastvideo-kernel/build.sh
@@ -11,11 +11,6 @@ echo "Building fastvideo-kernel..."
 # Install build dependencies
 pip install scikit-build-core cmake ninja
 
-# Set TORCH_CUDA_ARCH_LIST only if not already set
-if [ -z "$TORCH_CUDA_ARCH_LIST" ]; then
-    export TORCH_CUDA_ARCH_LIST="9.0a"
-fi
-
 # Build and install
 # Use -v for verbose output
 pip install . -v --no-build-isolation
