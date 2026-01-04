e52b57d8fd649a7441b0d27cfda698b291996886
H1yori233 <hiyoriizumi233@gmail.com>
2026-01-04 11:59:39 -0800

    update path

--- preprocess.sh
+++ preprocess.sh
@@ -1,17 +1,26 @@
+#!/bin/bash
+
+GPU_NUM=1 # 2,4,8
+MODEL_PATH="Matrix-Game-2.0-Base-Diffusers"
+DATA_MERGE_PATH="footsies-dataset/merge.txt"
+OUTPUT_DIR="footsies-dataset/preprocessed/"
+
+export CUDA_VISIBLE_DEVICES=0
 export MASTER_ADDR=localhost
 export MASTER_PORT=29500
 export RANK=0
 export WORLD_SIZE=1
 
 python fastvideo/pipelines/preprocess/v1_preprocess.py \
-    --model_path /workspace/Matrix-Game-2.0-Base-Diffusers \
-    --data_merge_path /workspace/footsies-dataset/merge.txt \
-    --output_dir /workspace/footsies-dataset/preprocessed/ \
-    --preprocess_task matrixgame \
-    --num_frames 81 \
+    --model_path $MODEL_PATH \
+    --data_merge_path $DATA_MERGE_PATH \
+    --preprocess_video_batch_size 4 \
+    --seed 42 \
     --max_height 480 \
     --max_width 832 \
-    --preprocess_video_batch_size 1 \
-    --dataloader_num_workers 0 \
-    --samples_per_file 8 \
-    --flush_frequency 32
+    --num_frames 81 \
+    --dataloader_num_workers 4 \
+    --output_dir=$OUTPUT_DIR \
+    --samples_per_file 64 \
+    --flush_frequency 64 \
+    --preprocess_task matrixgame
