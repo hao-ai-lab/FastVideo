c6dc7e8181c435447880b0143de49b86fa5b4b63
Will Lin <wlsaidhi@gmail.com>
2026-01-07 03:18:48 -0800

    dit

--- fastvideo/__init__.py
+++ fastvideo/__init__.py
@@ -1,6 +1,16 @@
-from fastvideo.configs.pipelines import PipelineConfig
-from fastvideo.configs.sample import SamplingParam
-from fastvideo.entrypoints.video_generator import VideoGenerator
+import os
+
 from fastvideo.version import __version__
 
-__all__ = ["VideoGenerator", "PipelineConfig", "SamplingParam", "__version__"]
+_LIGHT_IMPORT = os.getenv("FASTVIDEO_LIGHT_IMPORT", "0") == "1"
+
+if not _LIGHT_IMPORT:
+    from fastvideo.configs.pipelines import PipelineConfig
+    from fastvideo.configs.sample import SamplingParam
+    from fastvideo.entrypoints.video_generator import VideoGenerator
+
+    __all__ = [
+        "VideoGenerator", "PipelineConfig", "SamplingParam", "__version__"
+    ]
+else:
+    __all__ = ["__version__"]
