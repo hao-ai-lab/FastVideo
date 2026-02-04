ae9c5d7fc018355eb82a157b6210b72e1cbb55ed
Vishruth Bharath <vishruthbharath@gmail.com>
2026-02-04 05:37:05 +0000

    add gen3c, still need to test

---
+++ fastvideo/pipelines/basic/gen3c/__init__.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: Apache-2.0
+"""
+GEN3C Pipeline for FastVideo.
+
+GEN3C is a 3D-informed world-consistent video generation model with precise camera control.
+"""
+
+from fastvideo.pipelines.basic.gen3c.cache_3d import (
+    Cache3DBase,
+    Cache3DBuffer,
+    forward_warp,
+    unproject_points,
+    project_points,
+)
+from fastvideo.pipelines.basic.gen3c.gen3c_pipeline import (
+    Gen3CPipeline,
+    Gen3CDenoisingStage,
+    Gen3CLatentPreparationStage,
+)
+
+__all__ = [
+    # 3D Cache
+    "Cache3DBase",
+    "Cache3DBuffer",
+    "forward_warp",
+    "unproject_points",
+    "project_points",
+    # Pipeline
+    "Gen3CPipeline",
+    "Gen3CDenoisingStage",
+    "Gen3CLatentPreparationStage",
+]
