0c41d1dab538ea26dbe1aa3ce5f66b0d4e220a73
Mihir Jagtap <mjagtap@wisc.edu>
2026-02-07 13:50:38 -0800

    update pipeline files

--- fastvideo/pipelines/basic/hunyuangamecraft/__init_.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-"""HunyuanGameCraft pipeline module."""
-
-from fastvideo.pipelines.basic.hunyuangamecraft.hunyuangamecraft_pipeline import (
-    HunyuanGameCraftPipeline,
-)
-
-__all__ = ["HunyuanGameCraftPipeline"]
