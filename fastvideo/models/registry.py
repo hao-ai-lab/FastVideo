8efeee8da210675fbee87b4cddb58f02620c279a
Mihir Jagtap <mjagtap@wisc.edu>
2026-02-07 14:04:26 -0800

    update rotary embedding and registry for gamecraft

--- fastvideo/models/registry.py
+++ fastvideo/models/registry.py
@@ -25,10 +25,10 @@ logger = init_logger(__name__)
 _TEXT_TO_VIDEO_DIT_MODELS = {
     "HunyuanVideoTransformer3DModel":
     ("dits", "hunyuanvideo", "HunyuanVideoTransformer3DModel"),
-    "HunyuanVideo15Transformer3DModel":
-    ("dits", "hunyuanvideo15", "HunyuanVideo15Transformer3DModel"),
     "HunyuanGameCraftTransformer3DModel":
     ("dits", "hunyuangamecraft", "HunyuanGameCraftTransformer3DModel"),
+    "HunyuanVideo15Transformer3DModel":
+    ("dits", "hunyuanvideo15", "HunyuanVideo15Transformer3DModel"),
     "HYWorldTransformer3DModel":
     ("dits", "hyworld", "HYWorldTransformer3DModel"),
     "WanTransformer3DModel": ("dits", "wanvideo", "WanTransformer3DModel"),
@@ -73,6 +73,7 @@ _IMAGE_ENCODER_MODELS: dict[str, tuple] = {
 _VAE_MODELS = {
     "AutoencoderKLHunyuanVideo":
     ("vaes", "hunyuanvae", "AutoencoderKLHunyuanVideo"),
+    "AutoencoderKLCausal3D": ("vaes", "gamecraftvae", "GameCraftVAE"),
     "AutoencoderKLHYWorld": ("vaes", "hyworldvae", "AutoencoderKLHYWorld"),
     "AutoencoderKLHunyuanVideo15": ("vaes", "hunyuan15vae", "AutoencoderKLHunyuanVideo15"),
     "AutoencoderKLWan": ("vaes", "wanvae", "AutoencoderKLWan"),
