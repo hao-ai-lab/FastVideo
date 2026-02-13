6cae6a9d4f0e7ab89c0e1bc5a5b0a210f181c9bb
Mihir Jagtap <mjagtap@wisc.edu>
2026-02-12 18:29:58 -0800

    Update __init__.py
    remove duplicates

--- fastvideo/configs/pipelines/__init__.py
+++ fastvideo/configs/pipelines/__init__.py
@@ -6,9 +6,7 @@ from fastvideo.configs.pipelines.hunyuan import FastHunyuanConfig, HunyuanConfig
 from fastvideo.configs.pipelines.hunyuan15 import Hunyuan15T2V480PConfig, Hunyuan15T2V720PConfig
 from fastvideo.configs.pipelines.hunyuangamecraft import HunyuanGameCraftPipelineConfig
 from fastvideo.configs.pipelines.hyworld import HYWorldConfig
-from fastvideo.configs.pipelines.lingbotworld import LingBotWorldI2V480PConfig
 from fastvideo.configs.pipelines.ltx2 import LTX2T2VConfig
-from fastvideo.configs.pipelines.sd35 import SD35Config
 from fastvideo.registry import get_pipeline_config_cls_from_name
 from fastvideo.configs.pipelines.stepvideo import StepVideoT2VConfig
 from fastvideo.configs.pipelines.wan import (SelfForcingWanT2V480PConfig,
@@ -22,11 +20,4 @@ __all__ = [
     "WanT2V720PConfig", "WanI2V720PConfig", "StepVideoT2VConfig",
     "SelfForcingWanT2V480PConfig", "CosmosConfig", "Cosmos25Config",
     "LTX2T2VConfig", "HYWorldConfig", "get_pipeline_config_cls_from_name"
-    "HunyuanConfig", "FastHunyuanConfig", "PipelineConfig",
-    "Hunyuan15T2V480PConfig", "Hunyuan15T2V720PConfig", "SlidingTileAttnConfig",
-    "WanT2V480PConfig", "WanI2V480PConfig", "WanT2V720PConfig",
-    "WanI2V720PConfig", "StepVideoT2VConfig", "SelfForcingWanT2V480PConfig",
-    "CosmosConfig", "Cosmos25Config", "LTX2T2VConfig", "HYWorldConfig",
-    "SD35Config", "LingBotWorldI2V480PConfig",
-    "get_pipeline_config_cls_from_name"
 ]
