d50819b2702fbc8d2c67d5a57a763783456e33f0
Zhang Peiyuan <a1286225768@gmail.com>
2026-02-21 16:06:55 -0800

    Upstream LTX2 Training (#1116)
    Co-authored-by: RandNMR73 <notomatthew31@gmail.com>
    Co-authored-by: JerryZhou54 <zhouw.jerry2017@outlook.com>
    Co-authored-by: Davids048 <jundasu@ucsd.edu>
    Co-authored-by: Peiyuan Zhang <a1286225768@slurm-h200-204-239.slurm-compute.tenant-slurm.svc.cluster.local>

--- examples/training/finetune/ltx2/preprocess_ltx2_data_t2v_new.sh
+++ examples/training/finetune/ltx2/preprocess_ltx2_data_t2v_new.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-GPU_NUM=1
-MODEL_PATH="Davids048/LTX2-Base-Diffusers"
-# DATASET_PATH="data/overfit"
+GPU_NUM=4
+MODEL_PATH="FastVideo/LTX2-Distilled-Diffusers"
 DATASET_PATH="data/crush-smol"
 OUTPUT_DIR="$DATASET_PATH"
 WITH_AUDIO=true
 
-# Convert one-file overfit metadata into merged format if needed.
-if [ ! -f "$DATASET_PATH/videos2caption.json" ] && [ -f "$DATASET_PATH/overfit.json" ]; then
-  python scripts/dataset_preparation/convert_to_merged_dataset.py \
-    --items-json "$DATASET_PATH/overfit.json" \
-    --output-dir "$DATASET_PATH"
-fi
 
-
-torchrun --nproc_per_node=$GPU_NUM \
+torchrun  --nproc_per_node=$GPU_NUM \
     --master_port=29513 \
     -m fastvideo.pipelines.preprocess.v1_preprocessing_new \
     --model_path $MODEL_PATH \
@@ -28,8 +20,8 @@ torchrun --nproc_per_node=$GPU_NUM \
     --preprocess.with_audio $WITH_AUDIO \
     --preprocess.preprocess_video_batch_size 1 \
     --preprocess.dataloader_num_workers 0 \
-    --preprocess.max_height 480 \
-    --preprocess.max_width 832 \
-    --preprocess.num_frames 73 \
-    --preprocess.train_fps 16 \
+    --preprocess.max_height 1088  \
+    --preprocess.max_width 1920 \
+    --preprocess.num_frames 121 \
+    --preprocess.train_fps 24 \
     --preprocess.video_length_tolerance_range 5
