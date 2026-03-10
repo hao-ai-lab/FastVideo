3d8fc96c2625bf42f439f9992041e4925634299d
Peiyuan Zhang <a1286225768@gmail.com>
2026-03-10 00:37:53 +0000

    fix pin_memory_device warning

--- fastvideo/dataset/parquet_dataset_map_style.py
+++ fastvideo/dataset/parquet_dataset_map_style.py
@@ -392,7 +392,6 @@ def build_parquet_map_style_dataloader(
         collate_fn=passthrough,
         num_workers=num_data_workers,
         pin_memory=True,
-        pin_memory_device=current_platform.device_name,
         persistent_workers=num_data_workers > 0,
     )
     return dataset, loader
