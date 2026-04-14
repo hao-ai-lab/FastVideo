9081eb7bc1f8af5ecfcd9dcdf437a8ffb0a113f1
Mrinaal Dogra <mdogra@ucsd.edu>
2026-04-14 06:37:15 +0000

    [feat]: Add Z-Image Qwen3 Encoder and parity test

--- fastvideo/configs/models/encoders/__init__.py
+++ fastvideo/configs/models/encoders/__init__.py
@@ -4,6 +4,7 @@ from fastvideo.configs.models.encoders.clip import (CLIPTextConfig, CLIPVisionCo
 from fastvideo.configs.models.encoders.llama import LlamaConfig
 from fastvideo.configs.models.encoders.t5 import T5Config, T5LargeConfig
 from fastvideo.configs.models.encoders.qwen2_5 import Qwen2_5_VLConfig
+from fastvideo.configs.models.encoders.qwen3 import Qwen3Config
 from fastvideo.configs.models.encoders.siglip import SiglipVisionConfig
 from fastvideo.configs.models.encoders.reason1 import Reason1ArchConfig, Reason1Config
 from fastvideo.configs.models.encoders.gemma import LTX2GemmaConfig
@@ -11,5 +12,5 @@ from fastvideo.configs.models.encoders.gemma import LTX2GemmaConfig
 __all__ = [
     "EncoderConfig", "TextEncoderConfig", "ImageEncoderConfig", "BaseEncoderOutput", "CLIPTextConfig",
     "CLIPVisionConfig", "WAN2_1ControlCLIPVisionConfig", "LlamaConfig", "T5Config", "T5LargeConfig", "Qwen2_5_VLConfig",
-    "Reason1ArchConfig", "Reason1Config", "LTX2GemmaConfig", "SiglipVisionConfig"
+    "Reason1ArchConfig", "Reason1Config", "LTX2GemmaConfig", "SiglipVisionConfig", "Qwen3Config",
 ]
