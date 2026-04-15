f595bfe5c7ad2270a7659d9edd1d168c644a549d
SolitaryThinker <wlsaidhi@gmail.com>
2026-04-15 16:50:53 -0700

    remove empty files

--- fastvideo/configs/sample/hunyuangamecraft.py
+++
@@ -1,2 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Defaults now provided by profiles in fastvideo/pipelines/basic/gamecraft/profiles.py
--- fastvideo/configs/sample/ltx2.py
+++
@@ -1,2 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Defaults now provided by profiles in fastvideo/pipelines/basic/ltx2/profiles.py
