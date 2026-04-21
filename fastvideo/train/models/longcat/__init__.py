9c78ee38fa6169598493f47076f85679d6531289
Aryan Kumar <aryan5v@users.noreply.github.com>
2026-04-21 09:31:35 -0700

    [feat]: add LongCat self-forcing training scaffold

---
+++ fastvideo/train/models/longcat/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+"""LongCat model plugin package."""
+
+from fastvideo.train.models.longcat.longcat import (
+    LongCatModel as LongCatModel,
+)
+from fastvideo.train.models.longcat.longcat_causal import (
+    LongCatCausalModel as LongCatCausalModel,
+)
