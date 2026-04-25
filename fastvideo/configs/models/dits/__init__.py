0a8e9b3acc7b8174efc57274ce3fd094bb56eb3f
Raghav <ragg04@gmail.com>
2026-04-24 19:28:13 -0700

    [bugfix] Remove stray daVinci import from dits __init__
    davinci_magihuman.py does not exist on this branch; the import was
    accidentally introduced and breaks CI for any test that imports
    fastvideo.configs.models.dits.

--- fastvideo/configs/models/dits/__init__.py
+++ fastvideo/configs/models/dits/__init__.py
@@ -1,5 +1,4 @@
 from fastvideo.configs.models.dits.cosmos import CosmosVideoConfig
-from fastvideo.configs.models.dits.davinci_magihuman import DaVinciMagiHumanConfig
 from fastvideo.configs.models.dits.cosmos2_5 import Cosmos25VideoConfig
 from fastvideo.configs.models.dits.hunyuangamecraft import HunyuanGameCraftConfig
 from fastvideo.configs.models.dits.hunyuanvideo import HunyuanVideoConfig
@@ -11,7 +10,8 @@ from fastvideo.configs.models.dits.hyworld import HYWorldConfig
 from fastvideo.configs.models.dits.kandinsky5 import Kandinsky5VideoConfig
 
 __all__ = [
-    "HunyuanVideoConfig", "HunyuanVideo15Config", "HunyuanGameCraftConfig", "WanVideoConfig", "CosmosVideoConfig",
-    "Cosmos25VideoConfig", "LongCatVideoConfig", "LTX2VideoConfig", "HYWorldConfig", "Kandinsky5VideoConfig",
-    "DaVinciMagiHumanConfig",
+    "HunyuanVideoConfig", "HunyuanVideo15Config", "HunyuanGameCraftConfig",
+    "WanVideoConfig", "CosmosVideoConfig", "Cosmos25VideoConfig",
+    "LongCatVideoConfig", "LTX2VideoConfig", "HYWorldConfig",
+    "Kandinsky5VideoConfig",
 ]
