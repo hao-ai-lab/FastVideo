3f912bec01bc0df7631540e231dac159cfa8dca9
Davids048 <jundasu@ucsd.edu>
2026-05-08 17:57:14 -0700

    Add Dreamverse accelerator deps to extra

--- apps/dreamverse/docker/docker_build.sh
+++ apps/dreamverse/docker/docker_build.sh
@@ -6,8 +6,6 @@ REPO_ROOT="$(cd -- "${SCRIPT_DIR}/../../.." && pwd)"
 IMAGE="${DREAMVERSE_IMAGE:-dreamverse:dev}"
 
 build_args=()
-[[ -n "${FLASH_ATTN_REF:-}"  ]] && build_args+=(--build-arg "FLASH_ATTN_REF=${FLASH_ATTN_REF}")
-[[ -n "${FLASH_ATTN_REPO:-}" ]] && build_args+=(--build-arg "FLASH_ATTN_REPO=${FLASH_ATTN_REPO}")
 [[ -n "${CUDA_TAG:-}"        ]] && build_args+=(--build-arg "CUDA_TAG=${CUDA_TAG}")
 [[ -n "${BUILD_FASTVIDEO_KERNEL_FROM_SOURCE:-}" ]] && \
   build_args+=(--build-arg "BUILD_FASTVIDEO_KERNEL_FROM_SOURCE=${BUILD_FASTVIDEO_KERNEL_FROM_SOURCE}")
