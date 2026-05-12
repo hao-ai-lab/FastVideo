fc0bc551d705f4f8aad7fae2564d4ff6f23e5e14
SolitaryThinker <wlsaidhi@gmail.com>
2026-05-12 12:56:11 -0700

    [fix]: drop unused nn import in T5-Gemma encoder wrapper

--- fastvideo/models/encoders/t5gemma.py
+++ fastvideo/models/encoders/t5gemma.py
@@ -17,7 +17,6 @@ from __future__ import annotations
 import os
 
 import torch
-from torch import nn
 
 from fastvideo.configs.models.encoders import BaseEncoderOutput, TextEncoderConfig
 from fastvideo.models.encoders.base import TextEncoder
