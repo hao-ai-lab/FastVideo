4b0206d82a4f13a22046e42c5e9a570ccee59ae6
leffff <levnovitskiy@gmail.com>
2026-06-27 09:59:44 +0000

    [feat] add support for Kandinsky 5 Pro, Lite I2V

--- examples/inference/basic/basic_kandinsky5_i2v.py
+++ examples/inference/basic/basic_kandinsky5_i2v.py
@@ -1,13 +1,15 @@
 from fastvideo import VideoGenerator
 
-OUTPUT_PATH = "video_samples_kandinsky5_2B_i2v"
+OUTPUT_PATH = "video_samples_kandinsky5_i2v"
 
 IMAGE_PATH = "assets/girl.png"
 
 
 def main():
     generator = VideoGenerator.from_pretrained(
-        "kandinskylab/Kandinsky-5.0-I2V-Lite-sft-5s-Diffusers",
+        "kandinskylab/Kandinsky-5.0-I2V-Pro-distilled-5s-Diffusers", 
+        # "kandinskylab/Kandinsky-5.0-I2V-Pro-sft-5s-Diffusers"
+        # "kandinskylab/Kandinsky-5.0-I2V-Lite-5s-Diffusers"
         num_gpus=1,
         use_fsdp_inference=False,
         dit_cpu_offload=False,
@@ -18,18 +20,15 @@ def main():
     )
 
     prompt = (
-        "A woman sits at a wooden table by the window in a cozy café. She reaches out "
-        "with her right hand, picks up the white coffee cup from the saucer, and gently "
-        "brings it to her lips to take a sip. After drinking, she places the cup back on "
-        "the table and looks out the window, enjoying the peaceful atmosphere."
+        "A woman stands up and walks away"
     )
     _ = generator.generate_video(
         prompt,
         image_path=IMAGE_PATH,
         output_path=OUTPUT_PATH,
         save_video=True,
-        height=512,
-        width=768,
+        height=1024,
+        width=1024,
         num_frames=121,
     )
 
