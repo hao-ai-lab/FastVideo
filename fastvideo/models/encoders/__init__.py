1206b93765cbaa1f92878b4238937c3c66470c57
SolitaryThinker <wlsaidhi@gmail.com>
2026-06-27 20:21:59 -0700

    [bugfix]: include model namespaces in API docs

---
+++ fastvideo/models/encoders/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""Text, image, and multimodal encoder implementations."""
---
+++ fastvideo/models/vaes/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""Variational autoencoder implementations."""
