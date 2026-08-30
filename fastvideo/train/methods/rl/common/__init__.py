f8a292f3c1386d2aefb3a59e540451bb21823997
Adam <41438776+Abecid@users.noreply.github.com>
2026-08-30 12:29:12 -0700

    feat(rvm): export FastH3 RVM primitives

--- fastvideo/train/methods/rl/common/__init__.py
+++ fastvideo/train/methods/rl/common/__init__.py
@@ -1,14 +1,26 @@
 # SPDX-License-Identifier: Apache-2.0
 """Reusable RL training primitives."""
 
-from fastvideo.train.methods.rl.common.sampling import (
-    DiffusionSampler,
-    SamplingConfig,
-    SamplingResult,
+from fastvideo.train.methods.rl.common.minimax_h3_rvm import (
+    H3RVMSamplingConfig,
+    H3RVMSamplingResult,
+    MiniMaxH3RVMSampler,
 )
 from fastvideo.train.methods.rl.common.prompt_sampling import (
     KRepeatSample,
     distributed_k_repeat_indices,
+)
+from fastvideo.train.methods.rl.common.rvm_utils import (
+    detached_rvm_surrogate,
+    five_percent_interval,
+    partition_indices,
+    standardize_group_rewards,
+    visual_text_from_h3_prompt,
+)
+from fastvideo.train.methods.rl.common.sampling import (
+    DiffusionSampler,
+    SamplingConfig,
+    SamplingResult,
 )
 from fastvideo.train.methods.rl.common.validation import (
     RLValidationConfig,
@@ -19,12 +31,20 @@ from fastvideo.train.methods.rl.common.validation import (
 
 __all__ = [
     "DiffusionSampler",
+    "H3RVMSamplingConfig",
+    "H3RVMSamplingResult",
     "KRepeatSample",
+    "MiniMaxH3RVMSampler",
     "RLValidationConfig",
     "SamplingConfig",
     "SamplingResult",
+    "detached_rvm_surrogate",
     "distributed_k_repeat_indices",
+    "five_percent_interval",
     "media_to_video_array",
+    "partition_indices",
+    "standardize_group_rewards",
     "validation_caption",
     "validation_shard_indices",
+    "visual_text_from_h3_prompt",
 ]
