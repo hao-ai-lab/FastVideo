68329f477cd8da7691763f0d0f5ccdb68c2ae9bf
Adam <41438776+Abecid@users.noreply.github.com>
2026-09-02 14:13:48 -0700

    feat(h3-rest): export full-H3 REST teacher adapter

--- fastvideo/train/models/minimax_h3/__init__.py
+++ fastvideo/train/models/minimax_h3/__init__.py
@@ -6,6 +6,8 @@ from fastvideo.train.models.minimax_h3.minimax_h3 import (
 from fastvideo.train.models.minimax_h3.minimax_h3_dmd import (
     MiniMaxH3DMDModel as MiniMaxH3DMDModel, )
 from fastvideo.train.models.minimax_h3.minimax_h3_rest import (
-    MiniMaxH3RESTModel as MiniMaxH3RESTModel, )
+    MiniMaxH3RESTModel as MiniMaxH3RESTModel,
+    MiniMaxH3RESTTeacherModel as MiniMaxH3RESTTeacherModel,
+)
 from fastvideo.train.models.minimax_h3.minimax_h3_rvm import (
     MiniMaxH3RVMModel as MiniMaxH3RVMModel, )
